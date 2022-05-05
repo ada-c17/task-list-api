7b67617678fe89d17be8ec49dc6b7abc69562d8b
Jenny <jfperham@gmail.com>
2022-05-05 13:49:41 -0700

    set up dotenv folder, started to make Task Model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,8 @@
 from app import db
 
-
+#  title, description, and completed_at.
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.) #time_date, need to have option for null or None
