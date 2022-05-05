04dcaeab0408ec222f81865fc1dd78137888b5d6
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 18:42:46 -0500

    Created create_task POST endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, make_response
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+# create a new task
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    new_task = Task(title=request_body["title"], description=request_body["description"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response_body = {"task": new_task.to_dict()}
+
+    return make_response(jsonify(response_body), 201)
