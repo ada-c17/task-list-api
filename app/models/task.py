567bb958fa42b4eba880192f95c7abae4f60bfbf
Amel <amelntamark@gmail.com>
2022-05-05 14:04:54 -0700

    add nullable to Task.completed_at

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,4 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime)
+    completed_at = db.Column(db.DateTime, nullable=True)
