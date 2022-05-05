d85be6e801d55d022d0d6b4f98bf08c82ab04ad6
Becca <becca@adadevelopersacademy.org>
2022-05-05 13:50:18 -0700

    Passes wave 1 tests.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,31 @@
 from app import db
+from flask import abort, make_response, jsonify
 
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_json(self):
+        return {
+            'id': self.task_id,
+            'title': self.title,
+            'description': self.description,
+            'is_complete': self.completed_at != None
+        }
+    
+    @classmethod
+    def validate_id(cls, target_id):
+        try:
+            target_id = int(target_id)
+        except:
+            abort(make_response(jsonify(f"{target_id} is not a valid id."),400))
+        
+        task = cls.query.get(target_id)
+
+        if not task:
+            abort(make_response(jsonify(f"A task with id of {target_id} was not found."),404))
+        
+        return task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,58 @@
-from flask import Blueprint
+from os import abort
+from flask import Blueprint, jsonify, request, make_response, abort
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint('tasks', __name__, url_prefix = '/tasks')
+
+@task_bp.route('', methods = ['GET'])
+def get_all_tasks():
+    if not request.args:
+        all_tasks = [task.to_json() for task in Task.query.all()]
+    else:
+        all_tasks = [task.to_json() for task in Task.query.filter_by(**request.args)]
+
+    return jsonify(all_tasks), 200
+
+@task_bp.route('', methods = ['POST'])
+def create_task():
+    task_details = request.get_json()
+    # Validate and clean input
+    if 'title' not in task_details or 'description' not in task_details:
+        abort(make_response(jsonify({"details": "Invalid data"}),400))
+
+    new_task = Task(
+        title = task_details['title'],
+        description = task_details['description'],
+        completed_at = None
+        )
+    db.session.add(new_task)
+    db.session.commit()
+
+    return jsonify({'task': new_task.to_json()}), 201
+
+@task_bp.route('/<task_id>', methods = ['GET'])
+def get_task_by_id(task_id):
+    task = Task.validate_id(task_id)
+
+    return jsonify({'task': task.to_json()}), 200
+
+@task_bp.route('/<task_id>', methods = ['PUT'])
+def update_task(task_id):
+    task = Task.validate_id(task_id)
+    updated_details = request.get_json()
+    
+    for k,v in updated_details.items():
+        setattr(task, k, v)
+    
+    db.session.commit()
+
+    return jsonify({'task': task.to_json()}), 200
+
+@task_bp.route('/<task_id>', methods = ['DELETE'])
+def delete_task(task_id):
+    task = Task.validate_id(task_id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({'details': f'Task {task_id} "{task.title}" successfully deleted'}), 200
