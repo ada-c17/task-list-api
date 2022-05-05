484a5760db19dec5eb2fdf332871779b216aa553
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-05 18:43:26 -0500

    Added to_dict method to Task model, passed wave 01

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,15 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, default=None)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_dict(self):
+        if self.completed_at == None:
+            self.completed_at = False
+
+        return dict(
+            id=self.task_id,
+            title=self.title,
+            description=self.description,
+            is_complete=self.completed_at
+        )
