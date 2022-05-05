2c5466cc1372d37247c1e0e04cdf1a8f2d67bfd1
Emily Schell <lauraemilyschell@gmail.com>
2022-05-05 18:18:33 -0400

    adds task model and post route

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,17 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True, default=None)
+
+    def to_json(self):
+        return {
+            "task": {
+                "id": self.task_id,
+                "title": self.title,
+                "description": self.description,
+                "completed_at": self.completed_at
+            }
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,22 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request, abort
+
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+# POST ROUTES
+
+
+@tasks_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    if "title" not in request_body or "description" not in request_body:
+        return make_response("Invalid Request", 400)
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(new_task.to_json()), 201)
