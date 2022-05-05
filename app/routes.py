a1a2d20cc46d9c75033343988dcbc764d3fbf3b4
Liqingcathy <cathy.glc@gmail.com>
2022-05-05 14:21:00 -0700

    finish set up and get empty tasks

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,17 @@
-from flask import Blueprint
+from sys import prefix
+from flask import Blueprint, request, jsonify, make_response
+from .models.task import Task
+
+
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    
+    response_body = []
+    for task in tasks:
+        response_body.append(id=task.id, title=task.title, description=task.description)
+    
+    return jsonify(response_body), 200
+
