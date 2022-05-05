71d516165f96b62cbe6e1c53f4858281811cf4c4
Jenny <jfperham@gmail.com>
2022-05-05 15:32:08 -0700

    imported blueprint, initialized task_bp as a Blueeprint, registered bp in __init__.py and used tests to create outline of CRUD routes

--- app/models/task.py
+++ app/models/task.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from app import db
 
 #  title, description, and completed_at.
@@ -5,4 +6,4 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.) #time_date, need to have option for null or None
+    completed_at = db.Column(db.DateTime, default=None) #time_date, need to have option for null or None
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request, abort, make_response
+from .models.task import Task
+from app import db
+
+task_bp = Blueprint("task_bp",__name__, url_prefix="/tasks" )
+# Create: POST 
+def create_task():
+    pass 
+    # request body: {"title": "A Brand New Task", "description": "Test Description"}
+    # task Must contain title and description, else return: 
+    # {"details": "Invalid data"}, 400
+    # successful response body: {
+#         "task": {
+#             "id": 1,
+#             "title": "A Brand New Task",
+#             "description": "Test Description",
+#             "is_complete": False
+#         } }, 201
+
+# Read: GET
+def get_all_tasks():
+    pass
+def get_one_task(): # need this?
+    pass
+
+# example response body, status:
+#            { "id": 1,
+#             "title": "Go on my daily walk 🏞",
+#             "description": "Notice something new every day",
+#             "is_complete": False }, 200
+# 
+
+# Update: PUT
+def update_task():
+    pass
+# request body:
+#        { "title": "Updated Task Title",
+#         "description": "Updated Test Description"}
+
+#response_body == {"task": {
+#             "id": 1,
+#             "title": "Updated Task Title",
+#             "description": "Updated Test Description",
+#             "is_complete": False
+#         } }, 200
+
+
+# Delete: DELETE
+def delete_task():
+    pass
+# response body: {"details": 'Task 1 "Go on my daily walk 🏞" successfully deleted'}
+# 404 if not found
