2110fdda4ef1b3c74ac27e412708bc133a33c5c3
Uyen Vong <uvong92@gmail.com>
2022-05-05 14:11:54 -0700

    creates handle_tasks endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.task import Task
 
 tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
+
+@tasks_bp.route('', methods=['GET'])
+def handle_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+
+    for task in tasks:
+        tasks_response.append({
+            'title': task.title,
+            'description': task.description
+        })
+    return jsonify(tasks_response)
