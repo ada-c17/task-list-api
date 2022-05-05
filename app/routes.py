0e0f2b7409ef5f921976c93b5259ca83f8d4855c
Megan Korling <korlingm@gmail.com>
2022-05-05 14:28:38 -0700

    added task_bp blueprint and imported in create_app in init

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,6 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify, request, abort, make_response
+from app.models.task import Task
+from app import db
+
+task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
