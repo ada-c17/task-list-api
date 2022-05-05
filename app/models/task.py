4bcdd185cd0db0e3c80525e430422bd126de493f
Liqingcathy <cathy.glc@gmail.com>
2022-05-05 16:46:37 -0700

    post, get tests

--- app/models/task.py
+++ app/models/task.py
@@ -4,7 +4,7 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
+    title = db.Column(db.String) #index=True, unique=True with or without don't work for unique value
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
     
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,77 @@
-from sys import prefix
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, abort
 from .models.task import Task
-
+from app import db
 
 task_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
 
-@task_bp.route("", methods=["GET"])
-def get_tasks():
-    tasks = Task.query.all()
+def completed_or_not(response):
+    complete = "is_complete"
+    # [response] iterage in list of object, otherwise, TypeError
+    if complete in [response]: 
+        completed_status = True
+    else:
+        completed_status = False
+    return completed_status
+
+@task_bp.route("", methods=["POST"])
+def create_tasks():
+    response_body = request.get_json()
     
+    new_task = Task(title=response_body["title"], description=response_body["description"])
+    db.session.add(new_task)
+    db.session.commit()
+    
+    completed_status = completed_or_not(response_body)
+    return {
+        "task": {
+        "id": new_task.task_id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete": completed_status
+    }}, 201
+
+
+@task_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    completed_status = completed_or_not(tasks)
     response_body = []
     for task in tasks:
-        response_body.append(id=task.id, title=task.title, description=task.description)
+        response_body.append({
+            "id": task.task_id, 
+            "title":task.title, 
+            "description": task.description,
+            "is_complete": completed_status
+        })
     
     return jsonify(response_body), 200
 
+def task_validation(taskID):
+    try:
+        taskID = int(taskID)
+    except ValueError:
+        rsp = {"msg": f"Invalid task id #{taskID}."}
+        abort(make_response(jsonify(rsp), 400))
+    
+    valid_task = Task.query.get(taskID)
+    
+    if valid_task is None:
+        rsp = {"msg": f"Given task #{taskID} is not found."}
+        abort(make_response(jsonify(rsp), 404))
+
+    return valid_task
+
+@task_bp.route("/<taskID>", methods=["GET"])
+def get_one_task(taskID):
+    task_exist = task_validation(taskID)
+    completed_status = completed_or_not(task_exist)
+
+    rsp = {
+        "task": {
+        "id": task_exist.task_id,
+        "title": task_exist.title,
+        "description": task_exist.description,
+        "is_complete": completed_status
+    }}
+    return jsonify(rsp), 200
+    
