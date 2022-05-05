d4f7e919712fa8ff3118b71f3ecae5121d404bfd
Amber <ambertanaka@yahoo.com>
2022-05-05 15:28:35 -0700

    got post route working

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,19 @@
 from app import db
 from app.models.task import Task 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
 
-@tasks_bp.route("", methods=["POST"])
+@tasks_bp.route('', methods=['POST'])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title= request_body["title"],
-                    description=request_body["description"])
+    new_task = Task (title = request_body['title'],
+                    description = request_body['description'])
 
     db.session.add(new_task)
     db.session.commit()
 
     return {
-        'id' : new_task.id
+        'id' : new_task.task_id
     }, 201
