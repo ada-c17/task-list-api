731600b2fa4b6afae4266cb650392dc0e714d766
Amber <ambertanaka@yahoo.com>
2022-05-06 11:42:43 -0700

    made delete route and refactored code utilizing helper function to validate task ids

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
 from app import db
 from app.models.task import Task 
-from flask import Blueprint, jsonify, request, make_response
+from flask import Blueprint, jsonify, request, make_response, abort
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":f"task {task_id} not found"}, 404))
+
+    return task
 
 
 @tasks_bp.route('', methods=['POST'])
@@ -41,8 +54,8 @@ def list_all_tasks():
 
 @tasks_bp.route("/<task_id>", methods=['GET'])
 def get_one_task(task_id):
-    task = Task.query.get(task_id)
-    return{
+    task = validate_task(task_id)
+    return {
                 "task": {
                 "id":task.task_id,
                 "title":task.title,
@@ -53,8 +66,8 @@ def get_one_task(task_id):
 
 @tasks_bp.route("/<task_id>", methods=['PUT'])
 def update_one_task(task_id, ):
-    task = Task.query.get(task_id)
-    
+    task = validate_task(task_id)
+
     request_body = request.get_json()
     task.title = request_body["title"]
     task.description = request_body["description"]
@@ -68,3 +81,12 @@ def update_one_task(task_id, ):
                 "is_complete": False 
             }
     }
+
+@tasks_bp.route("/<task_id>", methods=['DELETE'])
+def delete_task(task_id):
+    task= validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+    return make_response(jsonify(f"details: Task {task.task_id},{task.description} successfully deleted."))
+
