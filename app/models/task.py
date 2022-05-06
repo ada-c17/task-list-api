b1da897f5e085cd337294cc33e7589593c65905c
olive <olivealavine@gmail.com>
2022-05-06 15:31:21 -0500

    started task routes and helper functions

--- app/models/task.py
+++ app/models/task.py
@@ -2,15 +2,27 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
 
     def to_dict(self):
         return dict(
-            task_id = self.task_id,
+            id = self.id,
             title = self.title,
             description = self.description,
-            completed_at = self.completed_at
+            is_complete = bool(self.completed_at)
         )
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(
+            title = data_dict["title"],
+            description = data_dict["description"],
+            is_complete = data_dict["is_complete"]
+        )
+
+    # def replace_details(self, data_dict):
+    #     self.title = data_dict["title"]
+    #     self.description = data_dict["description"]
+    #     self.completed_at = data_dict["completed_at"]
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,51 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.task import Task
+from app import db
+
+tasks_bp = Blueprint("bp", __name__, url_prefix="/tasks")
+
+def error_message(message, status_code):
+    def error_message(message, status_code):
+        abort(make_response(jsonify(dict(details=message)), status_code))
+
+def make_task_safely(data_dict):
+    try:
+        return Task.from_dict(data_dict)
+    except KeyError as err:
+        error_message(f"Missing key: {err}", 400)
+
+def get_task_record_by_id(id):
+    try:
+        id = int(id)
+    except ValueError:
+        error_message(f"Invalid id {id}", 400)
+
+    task = Task.query.get(id)
+
+    if task:
+        return task
+    
+    error_message(f"No task with id {id} found", 404)
+
+@tasks_bp.route("", methods = ["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    task = make_task_safely(request_body)
+
+    db.session.add(task)
+    db.session.commit()
+
+    return jsonify(f"Task {task.title} created successfully")
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    tasks = Task.query.all()
+    result_list = [task.to_dict() for task in tasks]
+
+    return jsonify(result_list)
+
+@tasks_bp.route("/<id>", methods=["Get"])
+def read_task_by_id(id):
+    task = get_task_record_by_id(id)
+    return jsonify(task.to_dict())
