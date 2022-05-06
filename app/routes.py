a4d4a5f3c974b3fb9b0cddca2596e53e6714fc3e
Amel <amelntamark@gmail.com>
2022-05-06 15:40:04 -0700

    make and register tasks blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,6 @@
 from os import abort
 from app import db
+from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, request
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
