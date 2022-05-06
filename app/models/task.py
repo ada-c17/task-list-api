5f703c58adcb3e89ca7732779e127636128ed505
Marlyn <marlynlopezz@gmail.com>
2022-05-05 21:43:35 -0700

    Task model created

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,9 @@
+from cgitb import text
 from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, default=None)
