07676cab8aba879484d758d529f74fe8924eeeb4
Amber <ambertanaka@yahoo.com>
2022-05-06 11:15:30 -0700

    made route to get one task by it's id

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,11 @@ from flask import Blueprint, jsonify, request, make_response
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix = "/tasks")
 
+
 @tasks_bp.route('', methods=['POST'])
 def create_task():
     request_body = request.get_json()
-    new_task = Task (title = request_body['title'],
+    new_task = Task(title = request_body['title'],
                     description = request_body['description'])
 
     db.session.add(new_task)
@@ -17,8 +18,37 @@ def create_task():
     return {
         "task": {
         "id": new_task.task_id,
-        "title": "A Brand New Task",
-        "description": "Test Description",
+        "title": new_task.title,
+        "description": new_task.description,
         "is_complete": False
     }
 },201
+
+@tasks_bp.route('', methods=['GET'])
+def list_all_tasks():
+    tasks = Task.query.all()
+    task_response = []
+    for task in tasks:
+        task_response.append(
+            {
+                "id":task.task_id,
+                "title":task.title,
+                "description":task.description,
+                "is_complete": False 
+            }
+        )
+    return jsonify(task_response)
+
+@tasks_bp.route("/<task_id>", methods=['GET'])
+def get_one_task(task_id):
+    task = Task.query.get(task_id)
+    return{
+                "task": {
+                "id":task.task_id,
+                "title":task.title,
+                "description":task.description,
+                "is_complete": False 
+            }
+    }
+
+
