b25e8d294a5f245665e7753058b4f9964bd43021
Megan Korling <korlingm@gmail.com>
2022-05-05 19:08:30 -0700

    added more assert statements to tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -61,6 +61,8 @@ def test_get_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body["details"] == "Task 1 does not exist."
+    tasks = Task.query.all()
+    assert tasks == []
     #raise Exception("Complete test with assertion about response body")
     # **Complete test with assertion about response body***************
     # *****************************************************************
@@ -131,6 +133,8 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
     assert response_body["details"] == "Task 1 does not exist."
+    tasks = Task.query.all()
+    assert tasks == []
     # raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
@@ -157,10 +161,12 @@ def test_delete_task_not_found(client):
     # Act
     response = client.delete("/tasks/1")
     response_body = response.get_json()
+    
 
     # Assert
     assert response.status_code == 404
     assert response_body["details"] == "Task 1 does not exist."
+    
     #raise Exception("Complete test with assertion about response body")
     # *****************************************************************
     # **Complete test with assertion about response body***************
