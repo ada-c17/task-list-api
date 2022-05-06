5d1b6c531c2ceb3b0bf80b8c9ba1a9692634f1a7
Emily Schell <lauraemilyschell@gmail.com>
2022-05-05 22:23:37 -0400

    wave 2 and 3 tests passed

--- app/models/task.py
+++ app/models/task.py
@@ -18,3 +18,13 @@ class Task(db.Model):
     def update(self, request_body):
         self.title = request_body["title"]
         self.description = request_body["description"]
+
+    @classmethod
+    def create(cls, request_body):
+        if "completed_at" not in request_body:
+            request_body["completed_at"] = None
+        new_task = cls(title=request_body["title"],
+                       description=request_body["description"],
+                       completed_at=request_body["completed_at"])
+
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from asyncio import tasks
 from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request, abort
 from .helpers import validate
+from datetime import date
 
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -14,8 +16,7 @@ def add_task():
     request_body = request.get_json()
     if "title" not in request_body or "description" not in request_body:
         return make_response({"details": "Invalid data"}, 400)
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
+    new_task = Task.create(request_body)
 
     db.session.add(new_task)
     db.session.commit()
@@ -25,24 +26,23 @@ def add_task():
 # GET ROUTES
 
 
-@tasks_bp.route("", methods=["GET"])
+@ tasks_bp.route("", methods=["GET"])
 def read_all_tasks():
-    # moons_query = request.args.get("moons")
-    # description_query = request.args.get("description")
+    sort_order = request.args.get("sort")
 
-    # if moons_query:
-    #     tasks = Planet.query.filter_by(moons=moons_query)
-    # elif description_query:
-    # planets = Planet.query.filter_by(description=description_query)
-    # else:
-    tasks = Task.query.all()
+    if sort_order == "asc":
+        tasks = Task.query.order_by(Task.title.asc())
+    elif sort_order == "desc":
+        tasks = Task.query.order_by(Task.title.desc())
+    else:
+        tasks = Task.query.all()
 
     response = [task.to_json() for task in tasks]
 
     return jsonify(response), 200
 
 
-@tasks_bp.route("/<task_id>", methods=["GET"])
+@ tasks_bp.route("/<task_id>", methods=["GET"])
 def read_one_task(task_id):
     task = validate(task_id)
     return make_response({"task": task.to_json()}, 200)
@@ -50,7 +50,7 @@ def read_one_task(task_id):
 # PUT ROUTES
 
 
-@tasks_bp.route("/<task_id>", methods=["PUT"])
+@ tasks_bp.route("/<task_id>", methods=["PUT"])
 def update_one_task(task_id):
     task = validate(task_id)
     request_body = request.get_json()
@@ -64,10 +64,30 @@ def update_one_task(task_id):
 # DELETE ROUTES
 
 
-@tasks_bp.route("/<task_id>", methods=["DELETE"])
+@ tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_one_planet(task_id):
     task = validate(task_id)
     db.session.delete(task)
     db.session.commit()
 
     return make_response({"details": f"Task {task_id} \"{task.title}\" successfully deleted"}, 200)
+
+# PATCH ROUTES
+
+
+@ tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_complete(task_id):
+    task = validate(task_id)
+    task.completed_at = date.today()
+
+    db.session.commit()
+    return make_response({"task": task.to_json()}, 200)
+
+
+@ tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_task_incomplete(task_id):
+    task = validate(task_id)
+    task.completed_at = None
+
+    db.session.commit()
+    return make_response({"task": task.to_json()}, 200)
