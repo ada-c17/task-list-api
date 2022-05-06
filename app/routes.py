90ea1467ba176587a9c0441cb68fe3b31c3281c9
Diana <arantxa.codes@gmail.com>
2022-05-05 18:19:27 -0700

    passed first test of wave 1, edited psycorp and pytest versions, added model and blueprint)

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
+from app.models.task import Task
+from app import db
+
+tasks_bp = Blueprint('tasks_bp', __name__, url_prefix = '/tasks')
+
+@tasks_bp.route("", methods = ["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    task_response = []
+    for task in tasks:
+        task_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.completed_at
+        })
+    return jsonify(task_response)
