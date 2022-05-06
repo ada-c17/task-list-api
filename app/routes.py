def8c21674d15226b0fa0fd79b4be22291b3c871
Ying G <yingg@yings-air.lan>
2022-05-06 11:41:03 -0700

    complete wave 1

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,108 @@
-from flask import Blueprint
+from wsgiref.util import request_uri
+from flask import Blueprint, jsonify, make_response, request, abort
+from app.models.task import Task
+from app import db
+import datetime
+import os
+
+tasks_bp = Blueprint("tasks",__name__,url_prefix="/tasks")
+
+@tasks_bp.route("",methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    response = []
+    if tasks:
+        for task in tasks:
+            response.append({
+                "id":task.task_id,
+                "title":task.title,
+                "description":task.description,
+                "is_complete":False if not task.completed_at else True
+            })
+    return make_response(jsonify(response),200)
+
+@tasks_bp.route("",methods=["POST"])
+def make_task():
+    request_body = request.get_json()
+    try:
+        new_task = Task(
+            title=request_body["title"],
+            description=request_body["description"]
+        )
+    except KeyError:
+        return make_response({"details":"Invalid data"},400)
+    db.session.add(new_task)
+    db.session.commit()
+    response = {
+        "task": {
+            "id":new_task.task_id,
+            "title":new_task.title,
+            "description":new_task.description,
+            "is_complete":False if not new_task.completed_at else True
+        }
+    }
+    return make_response(response,201)
+
+@tasks_bp.route("/<id>",methods=["GET"])
+def get_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return make_response({"details":"Invalid data"},400)
+    task = Task.query.get(id)
+    if not task:
+        return make_response({"details":f"Task #{id} does not exist"}, 404)
+    response = {
+        "task": {
+            "id":task.task_id,
+            "title":task.title,
+            "description":task.description,
+            "is_complete":False if not task.completed_at else True
+        }
+    }
+    return make_response(response,200)
+
+@tasks_bp.route("/<id>",methods=["PATCH","PUT"])
+def update_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return make_response({"details":"Invalid data"},400)
+    task = Task.query.get(id)
+    if not task:
+        return make_response({"details":f"Task #{id} does not exist"}, 404)
+    request_body = request.get_json()
+    if request.method=="PATCH":
+        if request_body.get("title"):
+            task.title = request_body["title"]
+        if request_body.get("description"):
+            task.description = request_body["description"]
+    else:
+        try:
+            task.title = request_body["title"]
+            task.description = request_body["description"]
+        except KeyError:
+            return make_response({"details":"Incomplete data"},400)
+    db.session.commit()
+    response = {
+        "task": {
+            "id":task.task_id,
+            "title":task.title,
+            "description":task.description,
+            "is_complete":False if not task.completed_at else True
+        }
+    }
+    return make_response(response,200)
+
+@tasks_bp.route("/<id>",methods=["DELETE"])
+def delete_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return make_response({"details":"Invalid data"},400)
+    task = Task.query.get(id)
+    if not task:
+        return make_response({"details":f"Task #{id} does not exist"}, 404)
+    db.session.delete(task)
+    db.session.commit()
+    return make_response({"details":f"Task {task.task_id} \"{task.title}\" successfully deleted"},200)
