b176ba73735b6c914600f9ab16427bba6a4f0cd3
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-05-06 16:38:23 -0700

    Working on fixing response bodies

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from os import abort
+
+from sqlalchemy import true
 from app import db
 from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, request
@@ -28,6 +30,24 @@ def validate_task(task_id):
     return task
 
 
+def format_response_body():
+    
+    task_instance = Task()
+
+    if task_instance.completed_at:
+        completed_task = True
+    else:
+        completed_task = False
+    
+    task_response_body = {
+        "id": task_instance.task_id,
+        "title": task_instance.title,
+        "description": task_instance.description,
+        "is_complete": completed_task
+    }
+
+    return task_response_body
+
 
 
 # ---- ROUTE FUNCTIONS ---- #
@@ -41,17 +61,59 @@ def validate_task(task_id):
 
 
 
+# ---- CREATE A TASK ---- #
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+
+    request_body = request.get_json()
+
+    new_task = Task(title=request_body["title"],
+                    description=request_body["description"])
+
+
+    # Returned response body should be a dictionary of dictionaries
+    # {
+    #     "task": {
+    #         "id": 1,
+    #         "title": "A Brand New Task",
+    #         "description": "Test Description",
+    #         "is_complete": false
+    #     }
+    # }
+
+    create_response_body = format_response_body(new_task)
+
+
+            # response_body = jsonify({ "task" :
+            #     {
+            #         "id" : new_task.task_id,
+            #         "title" : new_task.title,
+            #         "description" : new_task.description,
+            #         "is_complete" : False
+            #     }
+                
+            # })
+
+    response_body = jsonify()
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    # NEED TO FIX THIS RESPONSE
+    return make_response({response_body}, 201)
+
+
 # ---- GET ALL TASKS ---- #
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
     
-    tasks_response = []
+    response_body = []
 
     tasks = Task.query.all()
 
     for task in tasks:
         if task.completed_at == None:
-            tasks_response.append(
+            response_body.append(
                 {
                     "id" : task.task_id,
                     "title" : task.title,
@@ -60,7 +122,7 @@ def get_all_tasks():
                 }
             )
         else:
-            tasks_response.append(
+            response_body.append(
                 {
                     "id" : task.task_id,
                     "title" : task.title,
@@ -69,38 +131,47 @@ def get_all_tasks():
                 }
             )
 
-        # if task.completed_at == None:
-        #     tasks_response.append(
-        #         {
-        #             "id" : task.task_id,
-        #             "title" : task.title,
-        #             "description" : task.description,
-        #             "is_complete" : False
-        #         }
-        #     )
-        # else:
-        #     tasks_response.append(
-        #         {
-        #             "id" : task.task_id,
-        #             "title" : task.title,
-        #             "description" : task.description,
-        #             "is_complete" : True
-        #         }
-        #     )
+
+    # Returned response body should be a list of dictionaries:
+    # [
+    #     {
+    #         "id": 1,
+    #         "title": "Example Task Title 1",
+    #         "description": "Example Task Description 1",
+    #         "is_complete": false
+    #     },
+    #     {
+    #         "id": 2,
+    #         "title": "Example Task Title 2",
+    #         "description": "Example Task Description 2",
+    #         "is_complete": false
+    #     }
+    # ]
 
 
 
-    # return make_response({"task": tasks_response}, 201)
-    # return {"tasks": tasks_response}, 201
-    return jsonify(tasks_response)
+    # return make_response({"task": response_body}, 201)
+    # return {"tasks": response_body}, 201
+    return jsonify(response_body)
 
 
 
 
-# ---- GET ONE TASK BY ID ---- #
+# ---- GET ONE TASK ---- #
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
+
     task = validate_task(task_id)
+
+    # Return should be 
+    # {
+    #     "task": {
+    #         "id": 1,
+    #         "title": "Example Task Title 1",
+    #         "description": "Example Task Description 1",
+    #         "is_complete": false
+    #     }
+    # }
 
     return jsonify({"task" : 
         {
@@ -113,20 +184,29 @@ def get_one_task(task_id):
 
 
 
-# ---- CREATE A TASK ---- #
-# @tasks_bp.route("", methods=["POST"])
-# def create_task():
-    
-#     request_body = request.get_json()
-#     new_task = Task(title=request_body["title"],
-#                     description=request_body["description"])
+# ---- UPDATE ONE TASK ---- #
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def update_task(task_id):
 
-#     db.session.add(new_task)
-#     db.session.commit()
+    task = validate_task(task_id)
 
-#     # NEED TO FIX THIS RESPONSE
-#     return make_response({"task": new_task}, 201)
+    # Return should be 
+    # {
+    #     "task": {
+    #         "id": 1,
+    #         "title": "Example Task Title 1",
+    #         "description": "Example Task Description 1",
+    #         "is_complete": false
+    #     }
+    # }
 
+    return jsonify({"task" : 
+        {
+            "id" : task.task_id,
+            "title" : task.title,
+            "description" : task.description,
+        }
+    })
 
 
 
@@ -140,5 +220,12 @@ def delete_one_task(task_id):
     # Commit the changes
     db.session.commit()
 
+
+
+    # returned response body should look like this:
+    # {
+    #   "details": "Task 1 \"Go on my daily walk 🏞 \" successfully deleted"
+    # }
+
     # Need to change this response body  
-    return make_response({"details" : f'Task {task_id} successfully deleted.'}, 200)
+    # return make_response({"details" : f'Task {task_id} successfully deleted.'}, 200)
