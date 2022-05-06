9af7d112a770a0e15f0acb0834de475874ac76c2
Diana <arantxa.codes@gmail.com>
2022-05-05 20:53:20 -0700

    Wave 1 test 2 passed

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,4 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime)
+    completed_at = db.Column(db.DateTime, default = None)
--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,25 @@ from flask import Blueprint,jsonify
 from app.models.task import Task
 from app import db
 
-tasks_bp = Blueprint('tasks_bp', __name__, url_prefix = '/tasks')
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix = "/tasks")
 
 @tasks_bp.route("", methods = ["GET"])
 def get_all_tasks():
     tasks = Task.query.all()
     task_response = []
     for task in tasks:
-        task_response.append({
-            "id": task.task_id,
-            "title": task.title,
-            "description": task.description,
-            "is_complete": task.completed_at
-        })
+        if task.completed_at is None:
+            task_response.append({
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": False
+            })
+        else: 
+            task_response.append({
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": True
+            })
     return jsonify(task_response)
