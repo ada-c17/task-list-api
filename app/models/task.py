43e2ed0cfeb8e9c298d31d74c94b425729ddf559
olive <olivealavine@gmail.com>
2022-05-06 12:37:58 -0500

    added to_dictionary function in task model

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,12 @@ class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable=True)
+    completed_at = db.Column(db.DateTime)
+
+    def to_dict(self):
+        return dict(
+            task_id = self.task_id,
+            title = self.title,
+            description = self.description,
+            completed_at = self.completed_at
+        )
