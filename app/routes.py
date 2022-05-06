8db767518e5cf4976849741e6a9589b2366bf890
Ying G <yingg@yings-air.lan>
2022-05-06 12:45:08 -0700

    complete wave 3

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,6 @@ tasks_bp = Blueprint("tasks",__name__,url_prefix="/tasks")
 @tasks_bp.route("",methods=["GET"])
 def get_tasks():
     sort_query = request.args.get("sort")
-    print(sort_query)
     if sort_query:
         if sort_query.lower() == "desc":
             tasks = Task.query.order_by(Task.title.desc())
@@ -32,10 +31,12 @@ def get_tasks():
 @tasks_bp.route("",methods=["POST"])
 def make_task():
     request_body = request.get_json()
+    completed_at = request_body.get("completed_at",None)
     try:
         new_task = Task(
             title=request_body["title"],
-            description=request_body["description"]
+            description=request_body["description"],
+            completed_at=completed_at
         )
     except KeyError:
         return make_response({"details":"Invalid data"},400)
@@ -114,3 +115,45 @@ def delete_task(id):
     db.session.delete(task)
     db.session.commit()
     return make_response({"details":f"Task {task.task_id} \"{task.title}\" successfully deleted"},200)
+
+@tasks_bp.route("/<id>/mark_complete",methods=["PATCH"])
+def complete_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return make_response({"details":"Invalid data"},400)
+    task = Task.query.get(id)
+    if not task:
+        return make_response({"details":f"Task #{id} does not exist"}, 404)
+    task.completed_at = datetime.datetime.utcnow()
+    db.session.commit()
+    response = {
+        "task": {
+            "id":task.task_id,
+            "title":task.title,
+            "description":task.description,
+            "is_complete":False if not task.completed_at else True
+        }
+    }
+    return make_response(response,200)
+
+@tasks_bp.route("/<id>/mark_incomplete",methods=["PATCH"])
+def uncomplete_task(id):
+    try:
+        id = int(id)
+    except ValueError:
+        return make_response({"details":"Invalid data"},400)
+    task = Task.query.get(id)
+    if not task:
+        return make_response({"details":f"Task #{id} does not exist"}, 404)
+    task.completed_at = None
+    db.session.commit()
+    response = {
+        "task": {
+            "id":task.task_id,
+            "title":task.title,
+            "description":task.description,
+            "is_complete":False if not task.completed_at else True
+        }
+    }
+    return make_response(response,200)
