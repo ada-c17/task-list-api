d352f462623c8e97d3d5fb1fc8ea53fb465935c7
Gabriela Webb <gabw813@gmail.com>
2022-05-06 11:58:56 -0700

    added task model

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,7 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    task_title = db.Column(db.String)
+    task_description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
