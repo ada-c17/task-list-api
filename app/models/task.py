b02f9f6546a9ace335273caa438806d95e125a62
Morgan Buntin <mmbuntin@gmail.com>
2022-05-06 14:04:55 -0400

    started wave 1 work

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,13 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    @classmethod
+    def create(cls, req_body):
+        new_task = cls(
+            title=req_body["title"],
+            description=req_body["description"],
+            completed_at = (req_body["completed_at"] 
+                            if req_body["completed_at"] else None)
+        )
+        return new_task
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, abort
+from app import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", method=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    if request_body["title"] and request_body["description"]:
+        new_task = Task.create(request_body)
+    else:
+        abort(make_response({"message": f"Task data is not valid"}, 400))
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response(jsonify(f"New task {new_task.title} "\
+        "successfully created"), 201)
