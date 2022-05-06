8c02608efe66d9a28093419513288ddf6b86c32c
Diana <arantxa.codes@gmail.com>
2022-05-06 09:27:37 -0700

    Wave 01 4 passed

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
-from flask import Blueprint,jsonify
+from flask import Blueprint, jsonify, abort, make_response
 from app.models.task import Task
 from app import db
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix = "/tasks")
+
+def get_task_or_abort(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        response = {"msg": f"Invalid id: {task_id}"}
+        abort(make_response(jsonify(response), 400))
+
+    chosen_task = Task.query.get(task_id)    
+
+    if chosen_task is None:
+        response = {"msg": f"Could not find task with id {task_id}"}
+        abort(make_response(jsonify(response), 404))
+    return chosen_task
+
 
 @tasks_bp.route("", methods = ["GET"])
 def get_all_tasks():
@@ -23,4 +38,24 @@ def get_all_tasks():
                 "description": task.description,
                 "is_complete": True
             })
-    return jsonify(task_response)
+    return jsonify(task_response), 200
+
+@tasks_bp.route("/<task_id>", methods = ["GET"])
+def get_one_task(task_id):
+    chosen_task = get_task_or_abort(task_id)
+    task_dict= {}
+    if chosen_task.completed_at is None:
+        task_dict["task"] = {
+            "id": chosen_task.task_id,
+            "title": chosen_task.title,
+            "description": chosen_task.description,
+            "is_complete": False
+        }
+    else: 
+        task_dict["task"] = {
+            "id": chosen_task.task_id,
+            "title": chosen_task.title,
+            "description": chosen_task.description,
+            "is_complete": True
+        }
+    return jsonify(task_dict), 200
