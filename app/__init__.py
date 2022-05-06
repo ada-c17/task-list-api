7c81054633092a1e99c86942b2e126b88598468d
Roshni Patel <rpatel01@wesleyan.edu>
2022-05-05 20:00:15 -0700

    Wave 1 CRUD set up - couple of failing tests to debug

--- app/__init__.py
+++ app/__init__.py
@@ -25,10 +25,11 @@ def create_app(test_config=None):
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
-
+    from app.routes import tasks_bp
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-
+    app.register_blueprint(tasks_bp)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,115 @@
-from flask import Blueprint
+from attr import validate
+from flask import Blueprint, request,jsonify, make_response
+from app.models.task import Task
+from app import db 
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    if "description" not in request_body or "title" not in request_body:
+        return {"details": "Invalid data"}, 400
+
+
+    new_task = Task(title=request_body["title"], description=request_body["description"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": False
+        }
+    }, 201
+
+def is_complete(task):
+    if not task.completed_at:
+        return False 
+    else:
+        return True 
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        response = {"message": f"Could not retrieve task with id {task_id}"} 
+        return response, 404
+    
+    task = Task.query.get(task_id)
+
+    if task is None:
+        response = {"message": f"Invalid ID: {task_id}"}
+        return response, 400 
+    
+    return task 
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def get_task(task_id):
+    task = validate_task(task_id)
+
+    return make_response(jsonify({
+            "task": {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": is_complete(task)
+            }
+    })), 200
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    tasks = Task.query.all()
+    tasks_response = []
+
+    if len(tasks) == 0:
+        return jsonify(tasks_response), 200
+
+    for task in tasks:
+        tasks_response.append({
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": is_complete(task)
+        })
+    
+    return jsonify(tasks_response), 200
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    # updated_task = None
+    if "title" and "description" in request_body:
+        # updated_task.title = request_body["title"]
+        # updated_task.description = request_body["description"]
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+    
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": is_complete(task)
+        }
+    }, 200
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({
+        "details": f"Task {task_id} \"{task.title}\" successfully deleted"
+    }), 200
