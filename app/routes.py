14279f0663b1b75bd9ce30dbab441de01ec920a9
Sophia Wagner <sophiewagnerla@gmail.com>
2022-05-06 09:19:47 -0700

    Passed first test, registered BP and created GET route

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"])
+def get_books():
+    tasks = Task.query.all()
+    tasks_response = []
+    for task in tasks:
+        is_complete = True
+        if task.competed_at is None:
+            is_complete = False
+        tasks_response.append({"id": task.id, "title": task.title, "description": task.description, "is_complete": is_complete})
+    return jsonify(tasks_response)
+
