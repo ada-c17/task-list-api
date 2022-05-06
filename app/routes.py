c3591544134c6ce4187cc6733835a8c903083f67
Esther Annorzie <estherannorzie@gmail.com>
2022-05-06 19:38:50 -0400

    added Task attributes, registered Blueprint

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,5 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, make_response, request
+
+bp = Blueprint("tasks", __name__, url_prefix="/tasks")
