da8b4b9dba4801f4f256e24398db39958b67f9c5
Esther Annorzie <estherannorzie@gmail.com>
2022-05-07 12:52:09 -0400

    started working on create_task POST route

--- app/models/task.py
+++ app/models/task.py
@@ -6,3 +6,21 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+
+def to_dict(self):
+    return {
+        "task_id": self.task_id,
+        "title": self.title,
+        "description": self.description,
+        "is_complete": self.completed_at
+    }
+
+
+# @classmethod
+# def from_dict(cls, data_dict):
+#     return cls(
+#         title=data_dict["title"],
+#         description=data_dict["description"],
+#         completed_at=data_dict["completed_at"]
+#     )
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,27 @@
-from app import db
-from app.models.task import Task
 from flask import Blueprint, jsonify, make_response, request
+from app.models.task import Task
+from app import db
+
 
 bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+
+@bp.route("", methods=("POST",))
+def create_task():
+    request_body = request.get_json()
+    
+    new_task = Task(title=request_body["title"], description=request_body["description"])
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return make_response({
+        "task": {
+            "title": new_task.title, 
+            "description": new_task.description, 
+            "id": new_task.task_id, 
+            "is_complete": new_task.completed_at
+            }
+    }), 201
+
+    # return make_response(Task.to_dict(new_task))
