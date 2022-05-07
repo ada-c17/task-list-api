3b2c6856aee2112e90d6114637a3d9d749725bda
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-05-07 16:22:45 -0700

    Wave 03, created routes mark_complete and mark_imcomplete and refactored functions for posting and updating to update is_completed if a completed_at date was entered for task... see comments for potential refactoring down the line

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,27 @@ from flask import Blueprint, request, make_response, jsonify, abort
 from sqlalchemy import null
 from app import db
 from app.models.task import Task 
+import datetime
 
+def validate_task_id(task_id):
+    try: 
+        task_id = int(task_id)
+    except ValueError:
+        abort(make_response({"msg":f"The task with id {task_id} is invalid"}, 400))
+    
+    task = Task.query.get(task_id)
+    if task:
+        return task
+
+    abort(make_response({"msg":f"The task with id {task_id} is not found"}, 404))
+
+# def check_completed_at_status(task):
+#     task = validate_task_id(task)
+
+#     if task.completed_at:
+#         task.is_complete = True
+
+#     return task
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 # POST new task
@@ -16,15 +36,19 @@ def create_task():
         "description" not in request_body:
         return make_response({"details": "Invalid data"}), 400
 
-    new_task = Task(
-        title=request_body["title"], 
-        description=request_body["description"])
+    if "completed_at" in request_body:
+        new_task = Task(
+            title=request_body["title"], 
+            description=request_body["description"], 
+            completed_at=request_body["completed_at"], 
+            is_complete=True)
+    else:
+        new_task = Task(
+            title=request_body["title"], 
+            description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
-
-    if new_task.completed_at != null:
-        new_task.is_complete == True
 
     return jsonify({'task':
     {'id':new_task.task_id, 
@@ -55,24 +79,6 @@ def get_all_tasks():
 
     return jsonify(tasks_response)
 
-
-def validate_task_id(task_id):
-    try: 
-        task_id = int(task_id)
-    except ValueError:
-        abort(make_response({"msg":f"The task with id {task_id} is invalid"}, 400))
-    
-    task = Task.query.get(task_id)
-    if task:
-        return task
-
-    # tasks = Task.query.all()
-    # for task in tasks: 
-    #     if task.task_id == task_id:
-    #         return task
-
-    abort(make_response({"msg":f"The task with id {task_id} is not found"}, 404))
-
 # GET one task 
 @tasks_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
@@ -93,10 +99,10 @@ def update_task(task_id):
     task.title = request_body["title"]
     task.description = request_body["description"]
 
+    if task.completed_at: 
+        task.is_complete = True
+
     db.session.commit()
-    
-    if task.completed_at != null:
-        task.is_complete == True
 
     return jsonify({'task':
         {'id':task.task_id, 
@@ -108,8 +114,37 @@ def update_task(task_id):
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_task(task_id):
     task = validate_task_id(task_id)
-
     db.session.delete(task)
     db.session.commit()
 
     return make_response({"details": f"Task {task_id} \"{task.title}\" successfully deleted"})
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def mark_task_complete(task_id):
+    task = validate_task_id(task_id)
+
+    task.completed_at = datetime.datetime.now()
+    task.is_complete = True
+
+    db.session.commit()
+    return jsonify({'task':
+        {'id':task.task_id, 
+        'title':task.title, 
+        'description':task.description, 
+        'is_complete':task.is_complete}
+        })
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def mark_task_incomplete(task_id):
+    task = validate_task_id(task_id)
+
+    task.completed_at = None
+    task.is_complete = False
+    
+    db.session.commit()
+    return jsonify({'task':
+        {'id':task.task_id, 
+        'title':task.title, 
+        'description':task.description, 
+        'is_complete':task.is_complete}
+        })
