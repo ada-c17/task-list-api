0f27ae885c8857dd63efab54a2dd0cd830e8dc02
Roshni Patel <rpatel01@wesleyan.edu>
2022-05-07 11:37:53 -0700

    Establish one to many relationship between goal and task

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,5 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
+    tasks = db.relationship("Task", back_populates="goal", lazy=True) # go to task model and add goal
+    # lazy - not going to make instance until you try to use .tasks
