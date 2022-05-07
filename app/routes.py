79594f5539af61e8efa1411c70649aa58ae9a68d
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-05-07 12:03:12 -0700

    Created GET route to get one task at task_id endpoint, then refactored to take id validation logic out of the function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from operator import is_
+from xmlrpc.client import ResponseError
 from flask import Blueprint, request, make_response, jsonify, abort
 from app import db
 from app.models.task import Task 
@@ -20,14 +21,12 @@ def create_task():
     if new_task.completed_at != None:
         new_task.is_complete = True
 
-    response_body = jsonify({'task':
+    return jsonify({'task':
     {'id':new_task.task_id, 
     'title':new_task.title, 
     'description':new_task.description, 
     'is_complete':new_task.is_complete}
-    })
-
-    return response_body, 201
+    }), 201
 
 # GET all tasks
 @tasks_bp.route('', methods=["GET"])
@@ -44,3 +43,29 @@ def get_all_tasks():
         })
 
     return jsonify(tasks_response)
+
+
+def validate_task_id(task_id):
+    try: 
+        task_id = int(task_id)
+    except ValueError:
+        abort(make_response({"msg":f"The task with id {task_id} is invalid"}, 400))
+    
+    tasks = Task.query.all()
+    for task in tasks: 
+        if task.task_id == task_id:
+            return task
+
+    abort(make_response({"msg":f"The task with id {task_id} is not found"}, 404))
+
+# GET one task 
+@tasks_bp.route('/<task_id>', methods=["GET"])
+def get_one_task(task_id):
+    task = validate_task_id(task_id)
+
+    return jsonify({'task':
+    {'id':task.task_id, 
+    'title':task.title, 
+    'description':task.description, 
+    'is_complete':task.is_complete}
+    })
