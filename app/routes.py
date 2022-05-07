6521ee6be071f6175e16b0852b777aa0340e9783
Amel <amelntamark@gmail.com>
2022-05-07 12:05:23 -0700

    implement get_all_tasks

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,20 @@ from app.models.task import Task
 from flask import Blueprint, jsonify, abort, make_response, request
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+   tasks = Task.query.all()
+   response_body = []
+
+   for task in tasks:
+       response_body.append(
+           {
+               "id": task.id,
+               "title": task.title,
+               "description": task.description,
+               "completed_at": task.completed_at
+           }
+       )
+
+   return jsonify(response_body), 200
