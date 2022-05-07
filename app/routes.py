6c87ac1e2f24dba9c4efee7bf5d570a51619c1ba
Natascha <njenkns@gmail.com>
2022-05-07 00:07:10 -0700

    completed wave 3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.task import Task 
+from datetime import datetime 
 from flask import Blueprint, jsonify, abort, make_response, request
 
 tasks_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
@@ -20,13 +21,24 @@ def validate_task(task_id):
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    try:
-        new_task = Task(title=request_body["title"], 
-                    description=request_body["description"])
-    except KeyError:
-        return {
-            "details": "Invalid data"
-        }, 400
+    if "completed_at" not in request_body: 
+        try:
+            new_task = Task(title=request_body["title"], 
+                        description=request_body["description"])
+        except KeyError:
+            return {
+                "details": "Invalid data"
+            }, 400
+    else: 
+        try:
+            new_task = Task(title=request_body["title"], 
+                        description=request_body["description"],
+                        completed_at=request_body["completed_at"])
+        except KeyError:
+            return {
+                "details": "Invalid data"
+            }, 400
+    
     db.session.add(new_task)
     db.session.commit()
 
@@ -34,7 +46,7 @@ def create_task():
         "id": new_task.id,
         "title": new_task.title,
         "description": new_task.description,
-        "is_complete": False
+        "is_complete": isinstance(new_task.completed_at, datetime)
     }
 
     return make_response(jsonify({"task": response}), 201)
@@ -59,7 +71,7 @@ def read_all_tasks():
             "id": task.id, 
             "title": task.title,
             "description": task.description, 
-            "is_complete": False   
+            "is_complete": isinstance(task.completed_at, datetime)
             }
         )
     return make_response(jsonify(tasks_response))
@@ -73,7 +85,7 @@ def read_one_task(task_id):
             "id": task.id, 
             "title": task.title,
             "description": task.description, 
-            "is_complete": False
+            "is_complete": isinstance(task.completed_at, datetime)
     }
 
     return make_response(jsonify({"task": response}))
@@ -92,10 +104,30 @@ def update_task(task_id):
             "id": task.id, 
             "title": task.title,
             "description": task.description, 
-            "is_complete": False
+            "is_complete": isinstance(task.completed_at, datetime)
     }
 
     return make_response(jsonify({"task": response}))
+
+@tasks_bp.route("/<task_id>/<check_complete>", methods=["PATCH"])
+def update_is_complete(task_id, check_complete=None): 
+    task = validate_task(task_id)
+    if check_complete == "mark_complete":
+        task.completed_at = datetime.now()
+    else:
+        task.completed_at = None  
+    
+    db.session.commit()
+
+    response = {
+            "id": task.id, 
+            "title": task.title,
+            "description": task.description, 
+            "is_complete": isinstance(task.completed_at, datetime)
+    }
+
+    return make_response(jsonify({"task": response}))
+
 
 @tasks_bp.route("/<task_id>", methods=["DELETE"])
 def delete_one_task(task_id):
@@ -104,7 +136,7 @@ def delete_one_task(task_id):
     db.session.delete(task)
     db.session.commit()
     
-    response = (f'Task {task.id} "{task.title}" successfully deleted' )
+    response = (f'Task {task.id} "{task.title}" successfully deleted')
 
     return make_response(jsonify({"details": response})) 
 
