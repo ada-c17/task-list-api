e54a9ac04b2037fff332619832d56d909697017c
Amel <amelntamark@gmail.com>
2022-05-07 14:01:58 -0700

    unskip next test in wave 1 as it is passing

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -115,7 +115,7 @@ def test_update_task(client, one_task):
     assert task.completed_at == None
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_update_task_not_found(client):
     # Act
     response = client.put("/tasks/1", json={
@@ -127,7 +127,7 @@ def test_update_task_not_found(client):
     # Assert
     assert response.status_code == 404
 
-    raise Exception("Complete test with assertion about response body")
+    assert response_body == {"Message": "Task 1 not found"}
     # *****************************************************************
     # **Complete test with assertion about response body***************
     # *****************************************************************
