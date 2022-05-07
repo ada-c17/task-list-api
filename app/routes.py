3fabda769c6ff9bd2a0890cf754f4d5a171f926b
jeannie <jeanniezheng123@gmail.com>
2022-05-06 19:20:06 -0500

    completed wave 1

--- app/routes.py
+++ app/routes.py
@@ -15,8 +15,8 @@ def validate_task(task_id):
 
     task = Task.query.get(task_id)
 
-    if not Task:
-        return abort(make_response(jsonify(f"Planet {task_id} does not exist"), 404))
+    if not task:
+        return abort(make_response(jsonify(f"Task {task_id} does not exist"), 404))
     return task
 
 # @tasks_bp.route("", method=["POST"])
@@ -30,29 +30,26 @@ def validate_task(task_id):
 #     return make_response(jsonify(f"Task {new_task.title} successfully created"), 201)
 
 
-# class Task:
-#     def __init__(self, id, title, description):
-#         self.id = id
-#         self.title = title
-#         self.description = description
-
-
-# tasks = [
-#     Task(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-#     Task(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-#     Task(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-# ]
+# def completed_check(task_id):
+#     # task = validate_task(task_id)
+#     if task_id.completed_at == None:
+#         return False
+#     return True
 
 
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
     tasks_response = []
+    asc_query = request.args.get("title")
+    if asc_query:
+        tasks = Task.query.filter_by()
     tasks = Task.query.all()
+
     for task in tasks:
         tasks_response.append({
             "id": task.task_id,
             "title": task.title,
-            "description": task.description,
+            "description": task.description
         })
         if task.completed_at == None:
             tasks_response[0]["is_complete"] = False
@@ -62,39 +59,82 @@ def get_all_tasks():
 
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
-def get_one_task(task_id):
+def handle_task(task_id):
     task = validate_task(task_id)
+    return make_response(jsonify(task.to_json()), 200)
 
 
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"])
-    db.session.add(new_task)
+    # new_task = Task(title=request_body["title"],
+    #                 description=request_body["description"])
+
+    # new_task = Task.create(request_body)
+    try:
+        new_task = Task.create(request_body)
+        db.session.add(new_task)
+
+        db.session.commit()
+    except KeyError:
+        return make_response(jsonify({"details": "Invalid data"})), 400
+
+    # is_complete=request_body["is_complete"])
+    # db.session.add(new_task)
+    # db.session.commit()
+    return new_task.to_json(), 201
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    # try:
+    #     Task.update(request_body)
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    # # new_update = Task.update(request_body)
     db.session.commit()
-    return make_response(f"Task {new_task.title} successfully created", 201)
+    # except KeyError:
+    #     return abort(make_response(jsonify("Missing information")), 400)
 
-    # planet_query = request.args.get("name")
-    # color_query = request.args.get("color")
-    # if planet_query:
-    #     planets = Planet.query.filter_by(name=planet_query)
-    # elif color_query:
-    #     planets = Planet.query.filter_by(color=color_query)
-    # else:
-    tasks = Task.query.all()
+    return make_response(jsonify(task.to_json()), 200)
 
-    planets_response = [task.to_json() for task in tasks]
-# def get_all_tasks():
-#     tasks_response = []
-#     for task in tasks:
-#         tasks_response.append({
-#             "id": task.id,
-#             "title": task.title,
-#             "description": task.description
-#         })
-#     return jsonify(tasks_response)
 
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_one_task(task_id):
+    task = validate_task(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return make_response(jsonify({"details": f'Task {task_id} "{task.title}" successfully deleted'}), 200)
+
+    # "details": 'Task 1 "Go on my daily walk 🏞" successfully deleted'
+    # try:
+    #     Task.update(request_body)
+    #     db.session.commit()
+    # except KeyError:
+    #     return abort(make_response(jsonify("Missing information")), 400)
+
+    # return new_update.to_json(), 200
+    return {
+        "task": new_update.to_json()
+    }, 200
+
+
+"""
+    @books_bp.route("/<book_id>", methods=["PUT"])
+def update_book(book_id):
+    book = validate_book(book_id)
+
+    request_body = request.get_json()
+
+    book.title = request_body["title"]
+    book.description = request_body["description"]
+
+    db.session.commit()
+
+    return make_response(f"Book #{book.id} successfully updated")"""
 
 # @books_bp.route("", methods=["GET"])
 # def handle_books():
