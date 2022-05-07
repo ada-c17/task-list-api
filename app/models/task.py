be2a08cfea7c8c6e737cc4caf443b3d5d78f2687
Christina Webber <chrisswebb@yahoo.com>
2022-05-07 18:09:12 -0500

    get, post, and put

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,33 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    is_complete = False
+    completed_at = None
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            title=self.title,
+            description=self.description,
+            is_complete=self.is_complete)
+    
+    @classmethod
+    def from_dict(cls, data_dict):
+        return cls(
+            title=data_dict["title"],
+            description=data_dict["description"]
+            )
+    
+    def replace_details(self, data_dict):
+        self.title=data_dict["title"]
+        self.description=data_dict["description"]
+        return self.to_dict()
+
+    def mark_done(self):
+        self.is_complete = True
+        self.completed_at = db.func.current_timestamp()
+        return self.completed_at
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,65 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, make_response, request, abort
+from app import db
+from app.models.task import Task
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+def error_message(message, status_code):
+    abort(make_response(jsonify(dict(details=message)), status_code))
+
+def make_task_safely(data_dict):
+    try:
+        return Task.from_dict(data_dict)
+    except KeyError as err:
+        error_message(f"Missing key: {err}", 400)
+
+def update_task_safely(task, data_dict):
+    try:
+        return task.replace_details(data_dict)
+    except KeyError as err:
+        error_message(f"Missing key: {err}", 400)
+
+def validate_task_id(id):
+    try:
+        id = int(id)
+    except ValueError:
+        error_message(f"Invalid id {id}", 400)
+    task = Task.query.get(id)
+    if task:
+        return task
+    error_message(f"No task with ID {id}. SORRY.", 404)
+
+@task_bp.route("", methods=["POST"])
+def add_task():
+    request_body = request.get_json()
+    new_task=make_task_safely(request_body)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    task_response = {"task":new_task.to_dict()}
+
+    return jsonify(task_response), 201
+
+@task_bp.route("", methods=["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+
+    result_list = [task.to_dict() for task in tasks]
+    return jsonify(result_list)
+
+@task_bp.route("/<id>", methods=["GET"])
+def get_task_by_id(id):
+    task = validate_task_id(id)
+    result = {"task": task.to_dict()}
+    return jsonify(result)
+
+@task_bp.route("<id>", methods=["PUT"])
+def update_task(id):
+    task = validate_task_id(id)
+    request_body = request.get_json()
+    updated_task = update_task_safely(task, request_body)
+
+    db.session.commit()
+
+    return jsonify({"task":updated_task})
