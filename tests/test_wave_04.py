2898649b6a171ab91124530c9c3bbc11960c9e4f
Lin Liu <lin_6066@yahoo.com>
2022-05-08 13:45:36 -0700

    test post_slack_message

--- tests/test_wave_04.py
+++ tests/test_wave_04.py
@@ -1,1 +1,12 @@
 # There are no tests for wave 4.
+
+# test function post_slack_message
+from app.routes import post_slack_message
+import pytest
+
+@pytest.mark.skip
+def test_post_message_return_True():
+    assert post_slack_message("Hello World!") == True
+
+def test_post_empty_message_return_false():
+    assert post_slack_message("") == False
