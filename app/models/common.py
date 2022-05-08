69f9cfc6be2734d622118e82705a02bc749d1761
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-07 20:42:25 -0400

    cleanup

--- app/models/common.py
+++ app/models/common.py
@@ -9,12 +9,9 @@ def define_validation_on_model(func):
             target_id = int(target_id)
         except:
             abort(make_response(jsonify(f"{target_id} is not a valid id."),400))
-        
         target = cls.query.get(target_id)
-
         if not target:
             abort(make_response(jsonify(f"A {cls.__name__.lower()} with id of {target_id} was not found."),404))
-        
         return target
     
     return inner
