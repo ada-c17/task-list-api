fdc3db8401c36771cb77ae3e964ccc835b1f9287
maria <camilatagleo@gmail.com>
2022-05-08 21:09:46 -0700

    WAVE 01 fin ished

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,7 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None)
+    is_complete = db.Column(db.Boolean, default=False)
 
 # class Task:
 #     def __init__(self, id, title, description, completed_at=None):
--- app/routes.py
+++ app/routes.py
@@ -20,15 +20,26 @@ def validate_task_id(task_id):
     return the_task
 
 @tasks_bp.route("", methods=["POST"])
-def new_task():
+def create_task():
     request_body = request.get_json()
+
+    if "title" not in request_body or "description" not in request_body:
+        return make_response({"details": "Invalid data"}, 400)
+
     new_task = Task(title=request_body["title"],
                     description=request_body["description"])
 
     db.session.add(new_task)
     db.session.commit()
 
-    return make_response("201 CREATED", 201)
+
+    return make_response(jsonify({"task":{
+        "id": new_task.task_id,
+        "title": new_task.title,
+        "description": new_task.description,
+        "is_complete": new_task.is_complete
+    }
+    }), 201)
 
 @tasks_bp.route("", methods=["GET"])
 def get_all_tasks():
@@ -39,32 +50,52 @@ def get_all_tasks():
         tasks_response.append({
             "id": task.task_id,
             "title": task.title,
-            "description": task.description
+            "description": task.description,
+            "is_complete": task.is_complete
         })
     
     return jsonify(tasks_response)
 
 @tasks_bp.route("/<task_id>", methods=["GET"])
-def get_one_task(task_id):
+def get_task(task_id):
 
     task = validate_task_id(task_id)
 
-    return jsonify({
+    return jsonify({"task":{
+
         "id": task.task_id,
         "title": task.title,
-        "description": task.description
-        })
-    return make_response({"message": f'task with id {task_id} not found'}, 404)
+        "description": task.description,
+        "is_complete": task.is_complete
+        }})
+    
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task_id(task_id)
 
-# @tasks_bp.route("", methods=["POST"])
-# def create_task():
-#     request_body = request.get_json()
-#     new_task = Task(title=request_body["title"],
-#                     description=request_body["description"]
-#     )
+    request_body = request.get_json()
 
-#     db.session.add(new_task)
-#     db.session.commit()
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+    # task.completed_at = request_body["completed_at"]
+    # task.is_complete = request_body["is_complete"]
 
-#     return make_response("CREATED", 201)
+    db.session.commit()
 
+    return jsonify({"task":{
+        "id": task.task_id,
+        "title": task.title,
+        "description": task.description,
+        "is_complete": task.is_complete
+        }})
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task_id(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    task_deleted = f'Task {task.task_id} "{task.title}" successfully deleted'
+
+    return make_response({"details": task_deleted})
