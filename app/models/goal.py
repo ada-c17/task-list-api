79b4c54197075f1bf8242cd2408c9453b3ba4cf1
Sreymompech <sanchhen2013@gmail.com>
2022-05-09 22:53:00 -0700

    Add relationship to task table

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,4 +5,4 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
-   
+    tasks = db.relationship('Task', back_populates='goal', lazy=True)
