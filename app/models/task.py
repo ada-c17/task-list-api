48637a5e9d75e90075a6da53659ab5f618ec2ba2
Kassidy Buslach <kjbuslach@gmail.com>
2022-05-10 12:01:53 -0400

    wave1

--- app/models/task.py
+++ app/models/task.py
@@ -2,9 +2,28 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, default=None)
+    completed_at = db.Column(db.DateTime, default = False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description":self.description,
+            "completed_at": self.completed_at
+        }
+
+    def update_task(self, request_body):
+        self.title = request_body["title"]
+        self.description = request_body["description"]
+        self.completed_at = request_body["completed at"]
 
 
+    @classmethod
+    def create_task(cls, request_body):
+        new_task = cls(title = ["title"],
+            description = ["description"],
+            completed_at = ["completed"])
+        return new_task
