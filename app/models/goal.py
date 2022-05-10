b57dea554ae5b69b0fb4dcf2af8e8011cb6e997c
Julie <jawarren89@gmail.com>
2022-05-10 16:46:50 -0700

    adds goal model and migrations

--- app/models/goal.py
+++ app/models/goal.py
@@ -3,3 +3,18 @@ from app import db
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+
+    def return_goal_dict(self):
+        return {
+            "goal": {
+                "id": self.goal_id,
+                "title": self.title
+                }
+            }
+
+    def append_goal_dict(self):
+        return {
+                "id": self.goal_id,
+                "title": self.title
+        }
