cfb00088a044852ec2b359a8b6c94db5490b5c1a
Megan Korling <korlingm@gmail.com>
2022-05-10 13:19:27 -0700

    updated models, established one (goal) to many (task) relationship

--- app/models/task.py
+++ app/models/task.py
@@ -1,9 +1,13 @@
 from app import db
 
 
-class Task(db.Model):
+class Task(db.Model): # where do i put nullable=True?
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, default=None) 
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
+    goal = db.relationship("Goal", back_populates="tasks")
     
+
+    # make helper function for to dict
