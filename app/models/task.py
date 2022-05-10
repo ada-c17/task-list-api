b1aa59e731e423fcfc722a264c591aa1e93e4956
Sreymompech <sanchhen2013@gmail.com>
2022-05-09 22:54:17 -0700

    Add relationship with goal table

--- app/models/task.py
+++ app/models/task.py
@@ -5,4 +5,6 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=True)
+    goal = db.relationship("Goal", back_populates='tasks', lazy=True)
     
