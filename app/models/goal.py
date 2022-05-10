84f91ff49ffebfbbe9cdae90d2f9ae14638e721a
Sreymompech <sanchhen2013@gmail.com>
2022-05-09 22:38:02 -0700

    Set up goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,5 +1,8 @@
+
 from app import db
 
 
 class Goal(db.Model):
-    goal_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+   
