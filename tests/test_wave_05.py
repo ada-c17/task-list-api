6c7b7ebdf89d570d6abd3e16e0348eaa048c9b29
Grace <gpark439@gmail.com>
2022-05-11 14:46:00 -0700

    Update assertion test_delete_goal, pass Wave 05 all tests

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -115,7 +115,7 @@ def test_update_goal_not_found(client):
     assert response_body == {"message":f"Goal id '1' not found"}
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+#@pytest.mark.skip(reason="No way to test this feature yet")
 def test_delete_goal(client, one_goal):
     # Act
     response = client.delete("/goals/1")
@@ -130,10 +130,10 @@ def test_delete_goal(client, one_goal):
 
     # Check that the goal was deleted
     response = client.get("/goals/1")
-    assert response.status_code == 404
-    assert Goal.query.all() == []
-    assert response_body == {"message":f"Goal id '1' not found"}
+    response_body = response.get_json()
 
+    assert response.status_code == 404
+    assert response_body == {"message":f"Goal id '1' not found"}
 
 #@pytest.mark.skip(reason="test to be completed by student")
 def test_delete_goal_not_found(client):
