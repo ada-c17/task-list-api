02b098b0267ce9abf2b65d367c8a3bec1c44a157
Vida Maleki <vida.basmani@gmail.com>
2022-05-10 23:25:40 -0700

    wave 3 is done, makeing complete and incomplete rote

--- app/models/task.py
+++ app/models/task.py
@@ -1,8 +1,18 @@
 from app import db
-
+from datetime import datetime
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement= True)
     title = db.Column(db.String)
     description = db.Column(db.String)
-    completed_at = db.Column(db.DateTime, nullable = True)
+    completed_at = db.Column(db.DateTime, default = None)
+
+    def to_dict(self):
+        response_body = {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": bool(self.completed_at)
+        }
+        return response_body
+    
