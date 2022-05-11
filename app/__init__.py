6c5de6485847425ace586264a73ac9f6a1209ea4
Megan Korling <korlingm@gmail.com>
2022-05-11 10:24:52 -0700

    refactor routes into routes_task and routes_goals

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,8 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from .routes import tasks_bp, goals_bp
+    from .routes_tasks import tasks_bp
+    from .routes_goals import goals_bp
     app.register_blueprint(tasks_bp)
     app.register_blueprint(goals_bp)
 
