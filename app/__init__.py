f5096e75d137e021b7e70a02a75faa39e569687a
Elaine Smith <oneile1219@gmail.com>
2022-05-10 21:51:08 -0700

    Created routes folder and moved associated routes.py files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 from dotenv import load_dotenv
+import os
 
 
 db = SQLAlchemy()
@@ -14,7 +14,7 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    if test_config is None:
+    if not test_config:
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_DATABASE_URI")
     else:
@@ -22,15 +22,19 @@ def create_app(test_config=None):
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
             "SQLALCHEMY_TEST_DATABASE_URI")
 
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     # Import models here for Alembic setup
     from app.models.task import Task
     from app.models.goal import Goal
 
-    db.init_app(app)
-    migrate.init_app(app, db)
 
     # Register Blueprints here
-    from .routes import task_bp
+    from .routes.task_routes import task_bp
     app.register_blueprint(task_bp)
+
+    from .routes.goal_routes import goal_bp
+    app.register_blueprint(goal_bp)
 
     return app
