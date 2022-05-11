d42092153d150569a0190c62228198f5a39be607
Shannon Bellemore <shannonbellemore@gmail.com>
2022-05-11 18:39:11 -0500

    add make_dict function

--- app/models/task.py
+++ app/models/task.py
@@ -2,7 +2,15 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True)
+
+    def make_dict(self):
+        return {
+            "task_id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at
+        }
--- app/task_routes.py
+++ app/task_routes.py
@@ -4,13 +4,14 @@ from app.models.task import Task
 
 tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
-
 @tasks_bp.route("", methods=["POST"])
 def create_task():
     request_body = request.get_json()
-    new_task = Task(title=request_body["title"],
-                    description=request_body["description"],
-                    comleted_at=request_body["completed_at"])
+    new_task = Task(
+        title=request_body["title"],
+        description=request_body["description"],
+        completed_at=request_body["completed_at"]
+        )
 
     db.session.add(new_task)
     db.session.commit()
