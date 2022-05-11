086b4d49b499ca1a63792fe1bc01d9d99120e697
Sreymompech <sanchhen2013@gmail.com>
2022-05-10 20:21:03 -0700

    Adding code for Wave 6

--- app/routes/goals.py
+++ app/routes/goals.py
@@ -3,6 +3,7 @@ from urllib import response
 from flask import Blueprint, request, jsonify, abort, make_response
 from app import db
 from app.models.goal import Goal
+from app.models.task import Task
 
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
 
@@ -114,23 +115,67 @@ def delete_one_goal_id(goal_id):
     response_body = {"details": f'Goal {goal_id} "{chosen_goal.title}" successfully deleted'}
     return jsonify(response_body), 200
 
+
 #Wave 6
+# helper function to check task id
+def validate_task_id(task_id):
+    """
+    Checking the id task from input:
+        - return object task if id is integer
+        - raise exception if id is not integer then return status code 400,
+        but if the id not exist then return status code 404
+
+    """
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        abort(make_response({"message": f"The task id {task_id} is invalid. The id must be integer."}, 400))
+    
+    tasks = Task.query.all()
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    abort(make_response({"message": f"The task id {task_id} is not found"}, 404))
+
 @goals_bp.route("/<goal_id>/tasks", methods=["POST"]) 
 def post_task_ids_to_goal(goal_id):
-    chosend_goal = chosen_goal = validate_goal_id(goal_id)
+    # add task ids to goal
+    chosen_goal = validate_goal_id(goal_id)
     request_task_list = request.get_json()
-    if "task_list" in request_task_list:
-        tasks_list = request_task_list["task_list"]
-        for task_id in tasks_list:
-            if Goal.task.id == task_id:
-                Goal.task.goal_id = chosen_goal.id
-
+    if "task_ids" in request_task_list:
+        tasks_list = request_task_list["task_ids"]
+    for task_id in tasks_list:
+        chosen_task = validate_task_id(task_id)
+        if chosen_task.id == task_id and not chosen_task.goal_id:
+                chosen_task.goal_id = chosen_goal.id
     db.session.commit()
+
+    # get all task ids list for one goal
+    tasks = Task.query.all()
+    task_ids = []
+    for task in tasks:
+        if task.goal_id == chosen_goal.id:
+            task_ids.append(task.id)
     
     response_body = {
-        "id": Goal.task.goal_id,
-        "task_ids": tasks_list
+        "id": chosen_goal.id,
+        "task_ids": task_ids
     }
 
     return jsonify(response_body), 200
 
+
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_of_a_goal(goal_id):
+    chosen_goal = validate_goal_id(goal_id)
+    response_body = {
+        "id": chosen_goal.id,
+        "title": chosen_goal.title,
+        "tasks": []
+    }
+
+    for task in chosen_goal.tasks:
+        #if task.id == tasks.goal_id:
+        response_body["tasks"].append(task.task_response_body())
+    
+    return jsonify(response_body), 200
