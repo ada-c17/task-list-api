d1602ed3ce1cf635187f93e5745e77ff7b90b362
Juli <juli.terapin@gmail.com>
2022-05-11 18:13:04 -0400

    Fixed 'Update' method under Task Class'

--- app/models/task.py
+++ app/models/task.py
@@ -23,21 +23,20 @@ class Task(db.Model):
         return task_dict
 
     def update(self, request_body):
-        self.title=request_body['title']
-        self.description=request_body['description']
-
-        if self.completed_at:
-            self.completed_at=request_body['completed_at']
+        self.title=request_body["title"]
+        self.description=request_body["description"]
+        if self.completed_at in request_body:
+            self.completed_at=request_body["completed_at"]
     
     @classmethod
     def create(cls, request_body):
         if 'completed_at' in request_body:
-            new_task = Task(
+            new_task = cls(
                 title=request_body["title"],
                 description=request_body["description"],
                 completed_at=request_body["completed_at"])
         else:
-            new_task = Task(
+            new_task = cls(
                 title=request_body["title"],
                 description=request_body["description"])
         return new_task
