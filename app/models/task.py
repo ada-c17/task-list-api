88a1fef3c5a199f5f3be898c7af409f0a0364df6
Sreymompech <sanchhen2013@gmail.com>
2022-05-10 20:18:40 -0700

    Refactor code for route read_task_by_id by calling function in task class in returning

--- app/models/task.py
+++ app/models/task.py
@@ -1,3 +1,4 @@
+from urllib import response
 from app import db
 from app.models.goal import Goal
 class Task(db.Model):
@@ -7,4 +8,16 @@ class Task(db.Model):
     completed_at = db.Column(db.DateTime, default=None)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'), nullable=True)
     goal = db.relationship("Goal", back_populates='tasks', lazy=True)
+
+    def task_response_body(self):
+        response_task = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": bool(self.completed_at)
+        }
+        if self.goal_id:
+            response_task["goal_id"] = self.goal_id
+        return response_task
+
     
