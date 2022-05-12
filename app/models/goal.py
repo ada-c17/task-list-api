4803fe3e999491eac48591b928cbed50a4dcf9b9
Sreymompech <sanchhen2013@gmail.com>
2022-05-11 23:05:19 -0700

    Change relationship from back_populate to backref and create function for goal response to dictionary

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,8 +1,12 @@
-
 from app import db
-
-
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
-    tasks = db.relationship('Task', back_populates='goal', lazy=True)
+    tasks = db.relationship('Task', backref='goal', lazy=True)
+
+    def goal_response_body_dict(self):
+        response_goal = {
+            "id": self.id,
+            "title": self.title
+        }
+        return response_goal
