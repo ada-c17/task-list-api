f5833a0ebcbd87cc6161cace95cecc6eb4d98218
Danielle <dwhyte1226@gmail.com>
2022-05-12 17:00:28 -0500

    added post task list

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request, make_response, abort
 from app.models.goal import Goal
+from app.models.task import Task
 from app import db
 from app.helper import validate_id
 
@@ -51,3 +52,32 @@ def delete_goal(goal_id):
     db.session.delete(goal)
     db.session.commit()
     return jsonify(response), 200
+
+
+@goal_bp.route("/<goal_id>/tasks", strict_slashes=False,methods=["POST"])
+def add_task_list(goal_id):
+    goal = validate_id(Goal, goal_id)
+    request_body = request.get_json()
+
+    for task_id in request_body["task_ids"]:
+        task = Task.query.get(task_id)
+        task.goal_id = goal_id
+
+    db.session.commit()
+
+    return {"id": goal.goal_id,
+            "task_ids": [task.task_id for task in goal.tasks]}
+
+@goal_bp.route("/<goal_id>/tasks", strict_slashes=False,methods=["GET"])
+def get_goal_tasks(goal_id)
+# @goal_bp.route("/<goal_id>/tasks", strickt_slashes=False,methods=["POST"])
+# def create_task_on_goal(goal_id):
+#     goal = validate_id(Goal, goal_id)
+#     request_body = request.get_json()
+#     new_task = Task.fromdict(request_body)
+#     new_task.goal_id = goal.goal_id
+
+#     db.session.add(new_task)
+#     db.session.commit()
+
+
--- app/models/goal.py
+++ app/models/goal.py
@@ -4,7 +4,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    tasks = db.relationship("Task", back_populates="goal")
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
     def todict(self):
         return {"id": self.goal_id,
