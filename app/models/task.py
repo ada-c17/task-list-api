afd712a20f131dd3b10a78154e1fceb7abc424bd
Danielle <dwhyte1226@gmail.com>
2022-05-12 17:01:31 -0500

    added from dict method to task class

--- app/models/task.py
+++ app/models/task.py
@@ -6,7 +6,7 @@ class Task(db.Model):
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
-    goal_id = db.Column(db.Integer, ForeignKey('goal.goal_id'))
+    goal_id = db.Column(db.Integer, ForeignKey('goal.goal_id'), nullable=True)
     goal = db.relationship("Goal", back_populates="tasks")
 
     def todict(self):
@@ -14,3 +14,11 @@ class Task(db.Model):
                 "title": self.title,
                 "description": self.description,
                 "is_complete": bool(self.completed_at)}
+
+    @classmethod
+    def fromdict(cls, data_dict):
+        task = Task(title = data_dict["title"],
+                    description = data_dict["description"],
+                    completed_at = None,
+                    goal_id = data_dict.get("goal_id"))
+        return task
