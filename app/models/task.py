38d9f8aada233c55173478c242bdeb0e3334f11f
Gabriela Webb <gabw813@gmail.com>
2022-05-11 17:07:26 -0700

    adds POST route for single task

--- app/models/task.py
+++ app/models/task.py
@@ -3,6 +3,7 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    task_title = db.Column(db.String)
-    task_description = db.Column(db.String)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
     completed_at = db.Column(db.DateTime)
+    is_complete = db.Column(db.Boolean)
