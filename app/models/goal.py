ec36a157993370d02598fee07d27040d5369f835
camilla <pasadenastringtheory@gmail.com>
2022-05-12 12:48:15 -0700

    Create relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -10,6 +10,7 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal", lazy=True)
 
     def to_json(self):
         return {
