0031bc6b6e6d1d4eeaf2c324938fb29f0c6f3cfe
Danielle <dwhyte1226@gmail.com>
2022-05-12 02:04:13 -0500

    added get tasks to app/routes

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models.task import Task
+from app import db
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route("", strict_slashes=False, methods =["GET"])
+def get_tasks():
+    tasks = Task.query.all()
+    response = []
+    for task in tasks:
+        response.append({"id": task.task_id, 
+                        "title": task.title, 
+                        "description": task.description, 
+                        "is_complete": bool(task.completed_at)})
+    
+    return jsonify(response), 200
+
