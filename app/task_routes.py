e26fab60213d3b0d158cfebddba889338779ed07
Joanna Dudley <jfdudleybmc@gmail.com>
2022-05-12 15:35:51 -0400

    Added additional query parameter options to get_all_tasks, wrote associated helper functions, pytest fixtures, and tests

--- app/task_routes.py
+++ app/task_routes.py
@@ -22,22 +22,96 @@ def create_new_task():
 
     return success_message(dict(task=new_task.self_to_dict_no_goal()), 201)
 
+# Sorting helper functions
+
+def sort_tasks(all_tasks, sort_param):
+    if sort_param != "asc" and sort_param != "desc":
+        return error_message(f"Invalid sort parameter '{sort_param}'. Sort parameter may only be 'asc' or 'desc'.", 400)
+    elif sort_param == "asc":
+        sorted_tasks_asc = sorted(all_tasks, key = lambda i : i["title"])
+        return sorted_tasks_asc
+    elif sort_param == "desc":
+        sorted_tasks_desc = sorted(all_tasks, key = lambda i : i["title"], reverse=True)
+        return sorted_tasks_desc
+
+def filter_tasks_by_title(all_tasks, title_filter):
+    filtered_tasks = [task for task in all_tasks if title_filter.lower() in task["title"].lower()]
+    if filtered_tasks:
+            return filtered_tasks
+    else:
+        return error_message(f"Search parameter '{title_filter}' not found in any tasks.", 404)
+
+def filter_tasks_by_description(all_tasks, description_filter):
+    filtered_tasks = [task for task in all_tasks if description_filter.lower() in task["description"].lower()]
+    if filtered_tasks:
+            return filtered_tasks
+    else:
+        return error_message(f"Search parameter '{description_filter}' not found in any tasks.", 404)
+    
+
+def filter_tasks_by_title_and_description(all_tasks, title_filter, description_filter):
+    # didn't do a list comprehension for this one because there's so much going on it felt too difficult to read
+    filtered_tasks = []
+    for task in all_tasks:
+        if title_filter.lower() in task["title"].lower() and description_filter.lower() in task["description"].lower():
+            filtered_tasks.append(task)
+    if filtered_tasks:
+            return filtered_tasks
+    else:
+        return error_message(f"Search parameter(s) '{title_filter}' and/or '{description_filter}' not found in any tasks.", 404)
+
+
 
 @task_bp.route("", methods=["GET"])
 def get_all_tasks():
-    sort_param = request.args.get("sort")
     tasks = Task.query.all()
     all_tasks = [task.self_to_dict_no_goal() for task in tasks]
+    sort_param = request.args.get("sort")
+    description_param = request.args.get("description")
+    title_param = request.args.get("title")
 
-    if not sort_param:
+    # if no parameters
+    if not sort_param and not title_param and not description_param:
         return return_database_info_list(all_tasks)
-    if sort_param == "asc":
-        sorted_tasks_asc = sorted(all_tasks, key = lambda i : i["title"])
-        return return_database_info_list(sorted_tasks_asc)
-    if sort_param == "desc":
-        sorted_tasks_desc = sorted(all_tasks, key = lambda i : i["title"], reverse=True)
-        return return_database_info_list(sorted_tasks_desc)
 
+    # if sort param only
+    elif sort_param and not description_param and not title_param:
+        sorted_tasks = sort_tasks(all_tasks, sort_param)
+        return return_database_info_list(sorted_tasks)
+
+    # if description param only
+    elif description_param and not sort_param and not title_param:
+        filtered_tasks = filter_tasks_by_description(all_tasks, description_param)
+        return return_database_info_list(filtered_tasks)
+
+    # if title param only
+    elif title_param and not sort_param and not description_param:
+        filtered_tasks = filter_tasks_by_title(all_tasks, title_param)
+        return return_database_info_list(filtered_tasks)
+    
+    # if title and description param only
+    elif title_param and description_param and not sort_param:
+        double_filtered_tasks = filter_tasks_by_title_and_description(all_tasks, title_param, description_param)
+        return return_database_info_list(double_filtered_tasks)
+
+    # if sort and description param only
+    elif sort_param and description_param and not title_param:
+        filtered_tasks = filter_tasks_by_description(all_tasks, description_param)
+        sorted_tasks = sort_tasks(filtered_tasks, sort_param)
+        return return_database_info_list(sorted_tasks)
+    
+    # if sort and title param only
+    elif sort_param and title_param and not description_param:
+        filtered_tasks = filter_tasks_by_title(all_tasks, title_param)
+        sorted_tasks = sort_tasks(filtered_tasks, sort_param)
+        return return_database_info_list(sorted_tasks)
+    
+    # if all parameters
+    elif sort_param and title_param and description_param:
+        double_filtered_tasks = filter_tasks_by_title_and_description(all_tasks, title_param, description_param)
+        sorted_tasks = sort_tasks(double_filtered_tasks, sort_param)
+        return return_database_info_list(sorted_tasks)
+    
 
 @task_bp.route("/<task_id>", methods=["GET"])
 def get_one_task(task_id):
--- tests/test_wave_02.py
+++ tests/test_wave_02.py
@@ -82,3 +82,289 @@ def test_get_multiple_tasks_no_sorting_sorts_by_id(client, three_tasks):
             "is_complete": False,
             "title": "Pay my outstanding tickets 😭"}
     ]
+
+
+def test_get_tasks_invalid_sort_returns_error(client, three_tasks):
+    # Act
+    response = client.get("/tasks?sort=blah")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Invalid sort parameter 'blah'. Sort parameter may only be 'asc' or 'desc'."
+    }
+
+
+def test_get_tasks_filter_by_description(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?description=food")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 3
+    assert response_body == [
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"},
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"},
+        {
+            "description": "Make sure to eat food!",
+            "id": 5,
+            "is_complete": False,
+            "title": "Make dinner"}
+    ]
+
+def test_get_tasks_filter_by_description_param_not_found_returns_message(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?description=chocolate")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Search parameter 'chocolate' not found in any tasks."
+    }
+
+
+def test_get_tasks_filter_by_title(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"},
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"}
+    ]
+
+def test_get_tasks_filter_by_title_param_not_found_returns_message(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=chocolate")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Search parameter 'chocolate' not found in any tasks."
+    }
+
+
+def test_get_tasks_filter_by_title_and_description(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&description=give")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"},
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"}
+    ]
+
+def test_get_tasks_filter_by_title_and_description_not_found_returns_message(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=chocolate&description=honey")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Search parameter(s) 'chocolate' and/or 'honey' not found in any tasks."
+    }
+
+
+
+def test_get_tasks_filter_by_description_and_sort_ascending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?description=make&sort=asc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Make sure to eat food!",
+            "id": 5,
+            "is_complete": False,
+            "title": "Make dinner"},
+        {
+            "description": "Make sure the flowers grow!",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"}
+    ]
+
+def test_get_tasks_filter_by_description_and_sort_descending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?description=make&sort=desc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Make sure the flowers grow!",
+            "id": 1,
+            "is_complete": False,
+            "title": "Water the garden 🌷"},
+        {
+            "description": "Make sure to eat food!",
+            "id": 5,
+            "is_complete": False,
+            "title": "Make dinner"}
+    ]
+
+def test_get_tasks_filter_by_description_and_sort_fail_returns_error(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?description=make&sort=blah")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Invalid sort parameter 'blah'. Sort parameter may only be 'asc' or 'desc'."
+    }
+
+
+def test_get_tasks_filter_by_title_and_sort_ascending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&sort=asc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"},
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"}
+    ]
+
+def test_get_tasks_filter_by_title_and_sort_descending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&sort=desc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"},
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"}
+    ]
+
+def test_get_tasks_filter_by_title_and_sort_fail_returns_error(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&sort=blah")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Invalid sort parameter 'blah'. Sort parameter may only be 'asc' or 'desc'."
+    }
+
+def test_get_tasks_filter_by_title_and_description_sort_ascending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&description=give&sort=asc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"},
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"}
+    ]
+
+
+def test_get_tasks_filter_by_title_and_description_sort_descending(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&description=give&sort=desc")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body == [
+        {
+            "description": "Give cats wet food and pills",
+            "id": 3,
+            "is_complete": False,
+            "title": "Feed cats PM"},
+        {
+            "description": "Give cats wet and dry food",
+            "id": 2,
+            "is_complete": False,
+            "title": "Feed cats AM"}
+    ]
+
+def test_get_tasks_filter_by_title_and_description_sort_fail_returns_error(client, five_tasks_with_description):
+    # Act
+    response = client.get("/tasks?title=cats&description=give&sort=blah")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert "details" in response_body
+    assert response_body == {
+        "details": "Invalid sort parameter 'blah'. Sort parameter may only be 'asc' or 'desc'."
+    }
