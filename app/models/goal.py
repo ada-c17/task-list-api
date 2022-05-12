5db6afa64ceb42d2e1f85068c1e1597861b71263
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-05-11 20:46:10 -0700

    Created one to many relationship between goal and task, wrote helper methods within goal and task models

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,16 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+    tasks = db.relationship("Task", backref="goal")
+
+    def to_dict(self):
+        tasks_list = []
+        for task in self.tasks:
+            tasks_list.append(task.to_dict_basic())
+
+        return{
+            "id": self.id, 
+            "title": self.title, 
+            "tasks": tasks_list
+        }
