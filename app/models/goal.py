a002b0fa1c8f412cd5d02f7ab1958ef96a34365f
Huma Hameed <mrskq1@gmail.com>
2022-05-12 14:37:19 -0400

    established tasks/goal relationship

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,6 +5,7 @@ from app import db
 class Goal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
+    tasks = db.relationship("Task", back_populates="goal")
 
 
     def to_dict(self):
--- app/models/task.py
+++ app/models/task.py
@@ -8,6 +8,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
+    goal_id = db.Column(db.Integer, db.ForeignKey('goal.id'))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_json(self):
         # change name to_dict(self):
