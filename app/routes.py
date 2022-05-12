e9bde9db47f8a4fca0790d097711be3fd49520da
Myranae <myranae@pm.me>
2022-05-12 13:03:34 -0400

    undoes previous change

--- app/routes.py
+++ app/routes.py
@@ -181,7 +181,7 @@ def delete_goal(goal_id):
     goal = validate_id(goal_id)
     db.session.delete(goal)
     db.session.commit()
-    return make_response({"details": f"""Goal {goal_id} "{goal.title}" successfully deleted"""})
+    return make_response({"details": f'Goal {goal_id} "{goal.title}" successfully deleted'})
 
 @goals_bp.route("/<goal_id>/tasks", methods=["GET"])
 def get_tasks_from_one_goal(goal_id):
