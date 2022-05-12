e5e6e8a50bb458e72adafeaab0436e56107478df
Jamie H <jamiewhorvath@gmail.com>
2022-05-12 13:26:38 -0400

    first post and get all routes added

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,21 @@
+from email.policy import default
 from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String) #check for nullable=False
+    completed_at = db.Column(db.DateTime, default=None)
+
+    def to_json(self):
+
+        if not self.completed_at:
+            completed_at = False
+
+        return {
+                "id": self.task_id,
+                "title": self.title,
+                "description": self.description,
+                "is_complete": completed_at
+            }
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,32 @@
-from flask import Blueprint
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, abort, make_response, request
+
+
+task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
+
+@task_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+
+    try:
+        new_task = Task(title = request_body["title"], description = request_body["description"])
+        db.session.add(new_task)
+        db.session.commit()
+    except:
+        abort(make_response(jsonify({"message":f"invalid input"}), 400))
+    return make_response(jsonify({"task": new_task.to_json()}), 201)
+
+@task_bp.route("", methods=["GET"])
+def fetch_all_tasks():
+
+    # title_query = request.args.get("title")
+    # if title_query:
+    #     tasks = Task.query.filter_by(title_query)
+    # else:
+    tasks = Task.query.all()
+
+    task_response = []
+    for task in tasks:
+        task_response.append(task.to_json())
+    return make_response(jsonify(task_response),200)
