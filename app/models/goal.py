c997e732a98e6913fc4738b5f51e43c1b56fee4c
Diana <arantxa.codes@gmail.com>
2022-05-12 14:56:18 -0700

    added lazy and nullable comment

--- app/models/goal.py
+++ app/models/goal.py
@@ -5,3 +5,11 @@ class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     title = db.Column(db.String)
     tasks = db.relationship("Task", back_populates="goal")
+
+# class Goal(db.Model):
+#     goal_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    #title = db.Column(db.String)
+    #tasks = db.relationship("Task", back_populates="goal", lazy = True)
+
+# adding in lazy = True but could not update model due to migrations issue.
+
--- app/models/task.py
+++ app/models/task.py
@@ -9,3 +9,12 @@ class Task(db.Model):
     goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"))
     goal = db.relationship("Goal", back_populates="tasks")
 
+# class Task(db.Model):
+#     task_id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+#     title = db.Column(db.String)
+#     description = db.Column(db.String)
+#     completed_at = db.Column(db.DateTime, default = None)
+#     goal_id = db.Column(db.Integer, db.ForeignKey("goal.goal_id"), nullable=True)
+#     goal = db.relationship("Goal", back_populates="tasks")
+
+#adding in nullable = True as comment but could not update model due to migrations issue. 
