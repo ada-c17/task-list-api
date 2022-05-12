50ddcfc58c6c865d12d66f2bd50742e54280d69f
Megan Korling <korlingm@gmail.com>
2022-05-11 20:37:51 -0700

    added tests to test completed_at value for datetime object

--- tests/test_add_tests_all_waves.py
+++ tests/test_add_tests_all_waves.py
@@ -1,9 +1,15 @@
+from http.client import responses
 from app.models.task import Task
 from app.models.goal import Goal
 import pytest
-import unittest
-from unittest.mock import Mock, patch
 from app import db
+from datetime import datetime
+
+
+
+# Helper functions
+# would we want to write tests to check that validate_task and validate_goal 
+# are working correctly?
 
 
 # WAVE 1
@@ -27,16 +33,66 @@ def test_update_task_returns_400_with_no_description(client, one_task):
     task = Task.query.get(1)
     assert task.title == "Go on my daily walk 🏞"
 
-def test_update_goal_with_no_title_returns_400(client, one_goal):
-    response = client.put("goals/1")
+# datetime format 1: 'Sat, 07 May 2022 23:59:31 GMT'
+def test_create_tasks_takes_one_datetime_for_completed_at(client):
+    response = client.post("/tasks", json={
+        "title": "New task",
+        "description": "Description for new task",
+        "completed_at": 'Sat, 07 May 2022 23:59:31 GMT'
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert "task" in response_body
+    assert response_body["task"] == {
+        "id": 1,
+        "title": "New task",
+        "description": "Description for new task",
+        "is_complete": True
+    }
+    new_task = Task.query.get(1)
+    assert new_task
+    assert new_task.title == "New task"
+    assert new_task.description == "Description for new task"
+    assert new_task.completed_at == datetime.strptime('Sat, 07 May 2022 23:59:31 GMT', 
+                    '%a, %d %B %Y %H:%M:%S %Z')
+
+# datetime format 2: '2022-05-07 18:48:06.598253'
+def test_create_tasks_takes_second_datetime_for_completed_at(client):
+    response = client.post("/tasks", json={
+        "title": "New task",
+        "description": "Description for new task",
+        "completed_at": '2022-05-07 18:48:06.598253'
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert "task" in response_body
+    assert response_body["task"] == {
+        "id": 1,
+        "title": "New task",
+        "description": "Description for new task",
+        "is_complete": True
+    }
+    new_task = Task.query.get(1)
+    assert new_task
+    assert new_task.title == "New task"
+    assert new_task.description == "Description for new task"
+    assert new_task.completed_at == datetime.strptime('2022-05-07 18:48:06.598253', 
+                    '%Y-%m-%d %H:%M:%S.%f')
+
+def test_create_task_with_invalid_completed_at_returns_400(client):
+    response = client.post("/tasks", json={
+        "title": "New task",
+        "description": "Description for new task",
+        "completed_at": "Not a datetime"
+    })
     response_body = response.get_json()
 
     assert response.status_code == 400
-    assert len(response_body) == 1
-    assert response_body["details"] == "Goal needs a title"
-    goal = Goal.query.get(1)
-    assert goal.title == "Build a habit of going outside daily"
-
+    assert response_body["details"] == "Invalid date data"
+    tasks = Task.query.all()
+    assert len(tasks) == 0
 
 # WAVE 2
 def test_get_tasks_with_invalid_query_param_returns_400(client, three_tasks):
@@ -47,15 +103,16 @@ def test_get_tasks_with_invalid_query_param_returns_400(client, three_tasks):
     assert len(response_body) == 1
     assert response_body["details"] == "Invalid data"
 
-# WAVE 1 and 3 (?)
-# write tests to check that create_one_task adds in datetime if appropriate format
-# 1: 'Sat, 07 May 2022 23:59:31 GMT'
-# 2: '2022-05-07 18:48:06.598253'
-# 3: else, invalid type is 400
+# WAVE 5
+def test_update_goal_with_no_title_returns_400(client, one_goal):
+    response = client.put("goals/1")
+    response_body = response.get_json()
 
-# Helper functions
-# would we want to write tests to check that validate_task and validate_goal 
-# are working correctly?
+    assert response.status_code == 400
+    assert len(response_body) == 1
+    assert response_body["details"] == "Goal needs a title"
+    goal = Goal.query.get(1)
+    assert goal.title == "Build a habit of going outside daily"
 
 
 # WAVE 6 (and validate_task)
