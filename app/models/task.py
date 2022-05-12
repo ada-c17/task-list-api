aa7bb72bad19c6dfc4d9a3f406de33ca5878a597
Danielle <dwhyte1226@gmail.com>
2022-05-12 09:04:27 -0500

    updated description in task model to not be nullable

--- app/models/task.py
+++ app/models/task.py
@@ -3,5 +3,5 @@ from app import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=True)
+    description = db.Column(db.String, nullable=False)
     completed_at = db.Column(db.DateTime, nullable=True)
