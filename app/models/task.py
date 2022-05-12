3d698d1939ac302d578dbb869d21128f35d0faac
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-12 17:24:30 -0400

    Adds interpret_time function for string datetime conversion.

--- app/models/task.py
+++ app/models/task.py
@@ -1,6 +1,9 @@
+from inspect import getargvalues
+from operator import getitem
 from app import db
 from datetime import datetime
 from dateutil import parser
+from app.models.common import MissingValueError, FormatError
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
@@ -9,20 +12,27 @@ class Task(db.Model):
     completed_at = db.Column(db.DateTime, nullable=True)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
     goal = db.relationship('Goal', back_populates='tasks')
+    
+    @staticmethod
+    def interpret_timestamp(timestamp):
+        if type(timestamp) != str:
+            raise FormatError
+        try:
+            interpreted_time = parser.parse(timestr=timestamp)
+        except:
+            raise FormatError
+        return interpreted_time
 
     @classmethod
     def new_task(cls, task_details):
-        # Validate and clean input
+        # Validate and process input before creation
         if 'title' not in task_details or 'description' not in task_details:
-            raise ValueError
+            raise MissingValueError
         if 'completed_at' not in task_details:
             task_details['completed_at'] = None
-        elif not isinstance(task_details['completed_at'], datetime):
-            try:
-                task_details['completed_at'] = parser.parse(
-                                        timestr=task_details['completed_at'])
-            except:
-                raise TypeError
+        elif ((time := task_details.get('completed_at'))
+                and not isinstance(time, datetime)):
+            task_details['completed_at'] = Task.interpret_timestamp(time)
         
         return cls(
             title = task_details['title'],
@@ -31,13 +41,11 @@ class Task(db.Model):
         )
     
     def update(self, new_details):
-        if ('completed_at' in new_details 
-                and not isinstance(new_details['completed_at'], datetime)):
-            try:
-                new_details['completed_at'] = parser.parse(
-                                        timestr=new_details['completed_at'])
-            except:
-                raise TypeError
+        if ((time := new_details.get('completed_at'))
+                and not isinstance(time, datetime)):
+            new_details['completed_at'] = Task.interpret_timestamp(time)
+        
         for k,v in new_details.items():
             if k in self.__dict__:
                 setattr(self, k, v)
+    
