3ef92825b4911e3663f23ae2924a58144d7d281b
Amel <amelntamark@gmail.com>
2022-05-11 19:35:35 -0700

    add ability to delete goals

--- app/goals_routes.py
+++ app/goals_routes.py
@@ -79,3 +79,12 @@ def update_goal(goal_id):
     goal_dict = {"goal": make_goal_dict(goal)}
 
     return jsonify(goal_dict), 200
+
+@goals_bp.route("/<goal_id>", methods=["DELETE"])
+def delete_goal(goal_id):
+    goal = validate_goal(goal_id)
+
+    db.session.delete(goal)
+    db.session.commit()
+
+    return jsonify({"details": f'Goal {goal_id} "{goal.title}" successfully deleted'}), 200
