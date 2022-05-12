e166d4a3d3643b5bc9c57e72d532bf97f708c310
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-12 16:13:08 -0500

    Create to_dict() method for Goal model

--- app/models/goal.py
+++ app/models/goal.py
@@ -4,3 +4,11 @@ from app import db
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
+
+    def to_dict(self):
+        goal_dict = {
+            "id": self.goal_id,
+            "title": self.title
+        }
+
+        return goal_dict
