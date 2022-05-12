9b673c2b53326b91245e0145eb14bf59378fc486
Gabriela Webb <gabw813@gmail.com>
2022-05-11 18:33:35 -0700

    adds GET routes for tasks

--- app/routes.py
+++ app/routes.py
@@ -17,3 +17,34 @@ def create_task():
                     "title": new_task.title,
                     "description": new_task.description,
                     "is_complete": False}}, 201
+
+@tasks_bp.route("", methods=["GET"])
+def get_all_tasks():
+    response = []
+    tasks = Task.query.all()
+    for task in tasks:
+        response.append(
+            {"id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": False}
+        )
+
+    return jsonify(response)
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def one_task(task_id):
+    try:
+        task_id = int(task_id)
+    except ValueError:
+        return jsonify({"msg":f"Invalid task id: {task_id}. ID must be an integer."}), 400
+
+    requested_task = Task.query.get(task_id)
+
+    if requested_task is None:
+        return jsonify({"msg":f"Could not find task with id: {task_id}"}), 404
+    
+    return {"task": {"id": requested_task.task_id,
+                    "title": requested_task.title,
+                    "description": requested_task.description,
+                    "is_complete": False}}
