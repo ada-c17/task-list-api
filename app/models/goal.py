21821f611b06f9683c03ed56f5f0e503197f2e04
Hope Olaide <hopeolaide@gmail.com>
2022-05-13 17:49:38 -0400

    Database corrections.

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,14 +1,5 @@
 from app import db
 
-"""Answer the following questions. 
-These questions will help you become familiar with the API, 
-and make working with it easier.
-
-What is the responsibility of this endpoint?
-What is the URL and HTTP method for this endpoint?
-What are the two required arguments for this endpoint?
-How does this endpoint relate to the Slackbot API key (token) we just created?"""
-
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
