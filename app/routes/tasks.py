8e9f3f606235bc786fd75c4200eb219bc324e844
Natalya <chshebetun@gmail.com>
2022-05-12 17:56:31 -0700

    Requirements update

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -4,9 +4,8 @@ from app import db
 from datetime import datetime
 import logging
 import os
-import slack
-from slack import WebClient
-from slack.errors import SlackApiError
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger(__name__)
 client = WebClient(token=os.environ.get('SLACK_BOT_TOKEN'))
