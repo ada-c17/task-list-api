721921168456ec6c5a3af92e345aa01e3fd80a08
camilla <pasadenastringtheory@gmail.com>
2022-05-12 17:22:36 -0700

    Implement last functions, comment pytest skip decorators

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -1,11 +1,27 @@
 from flask import Blueprint, jsonify, make_response, abort, request
 from app.models.goal import Goal
+from app.models.task import Task
 from app import db
 # helper function file import
 
 
 goal_bp = Blueprint("goal_bp", __name__, url_prefix="/goals")
 
+'''
+HELPER FUNCTION
+'''
+
+def validate_goal(goal_id):
+    try:
+        goal_id = int(goal_id)
+    except:
+        abort(make_response({"details":f"goal {goal_id} invalid"}, 400))
+
+    goal = Goal.query.get(goal_id)
+    if not goal:
+        return abort(make_response({"details": f"Goal {goal_id} not found"}, 404))
+
+    return goal
 
 '''
 POST ROUTE
@@ -30,7 +46,6 @@ def create_goal():
     }
     return make_response(jsonify({"goal": goal_response_body}), 201)
 
-
 '''
 GET ROUTES
 # '''
@@ -50,19 +65,6 @@ def read_saved_goals():
             }
         )
     return jsonify(goals_response)
-
-
-def validate_goal(goal_id):
-    try:
-        goal_id = int(goal_id)
-    except:
-        abort(make_response({"details":f"goal {goal_id} invalid"}, 400))
-
-    goal = Goal.query.get(goal_id)
-    if not goal:
-        return abort(make_response({"details": f"Goal {goal_id} not found"}, 404))
-
-    return goal
 
 
 # GET ONE GOAL
@@ -110,3 +112,71 @@ def delete_one_goal(goal_id):
     delete_response = f"Goal {goal.goal_id} \"{goal.title}\" successfully deleted"
 
     return make_response(jsonify({"details": delete_response}), 200)
+
+
+'''
+ENDPOINT: /<goal_id>/tasks
+POST ROUTE
+'''
+
+'''
+Post goal with tasks to a given endpoint with a given goal id
+Input body: {"task_ids": [1, 2, 3]}
+Output expected: {"id": <goal_id>,"task_ids": [1, 2, 3]}
+'''
+
+# POST GOAL WITH TASKSs
+@goal_bp.route("/<goal_id>/tasks", methods=["POST"])
+def post_tasks_to_goal(goal_id):
+    request_body = request.get_json()
+
+    goal = validate_goal(goal_id)
+
+    for id in request_body["task_ids"]:
+        task = Task.query.get(id)
+        task.goal = goal
+
+    db.session.commit()
+
+    goal_response_body = {
+            "id": goal.goal_id,
+            "task_ids": request_body["task_ids"]
+    }
+
+    return make_response(jsonify(goal_response_body), 200)
+
+
+'''
+ENDPOINT: /<goal_id>/tasks
+GET ROUTE
+'''
+
+'''
+Get goal with or without tasks from given endpoint
+Output expected: {
+        "id": <goal_id>,
+        "title": "Build a habit of going outside daily",
+        "tasks": [
+            {
+                "id": 1,
+                "goal_id": 1,
+                "title": "Go on my daily walk 🏞",
+                "description": "Notice something new every day",
+                "is_complete": False
+            }
+        ]
+    }
+'''
+# GET GOAL WITH TASKS
+@goal_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_goal_and_tasks(goal_id):
+    goal = validate_goal(goal_id)
+
+    # list_of_tasks = [task.to_json() for task in goal.tasks]
+    goal_response_body = {
+            "id": goal.goal_id,
+            "title": goal.title,
+            "tasks": [task.to_json() for task in goal.tasks]
+    }
+
+    return make_response(jsonify(goal_response_body), 200)
