bf93f05cd2c50c11028d8aba0e862a8452ad5530
Esther Annorzie <estherannorzie@gmail.com>
2022-05-13 02:59:58 -0400

    added assertion for getting  goals with no tasks

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -33,20 +33,29 @@ def add_tasks_to_goal(goal_id):
         tasks_list.append(task.task_id)
 
     db.session.commit()
-    return make_response(jsonify({"id": goal.goal_id, "task_ids": tasks_list}))
+    return make_response(jsonify({"id": goal.goal_id, 
+    "task_ids": tasks_list}))
     
 
 @bp.route("/<goal_id>", methods=("GET",))
 def read_goal(goal_id):
     goal = validate_goal_id(goal_id)
-    return make_response(jsonify({"goal": goal.to_dict()}), 200)
+    return make_response(jsonify({"goal": goal.to_dict()}))
 
 
 @bp.route("", methods=("GET",))
 def read_all_goals():
     goals = Goal.query.all()
     goals_response = [goal.to_dict() for goal in goals]
-    return make_response(jsonify(goals_response), 200)
+    return make_response(jsonify(goals_response))
+
+
+@bp.route("/<goal_id>/tasks", methods=("GET",))
+def read_specific_goal_tasks(goal_id):
+    goal = validate_goal_id(goal_id)
+    task = validate_task_id
+    goal_tasks = [task for task in goal.tasks]
+    pass
 
 
 @bp.route("/<goal_id>", methods=("PUT",))
@@ -56,7 +65,7 @@ def replace_goal(goal_id):
     goal.override_goal(request_body)
     db.session.commit()
 
-    return jsonify({"goal": goal.to_dict()}), 200
+    return jsonify({"goal": goal.to_dict()})
 
 
 @bp.route("/<goal_id>", methods=("DELETE",))
@@ -65,4 +74,4 @@ def delete_goal(goal_id):
     db.session.delete(goal)
     db.session.commit()
 
-    create_message(f'Goal {goal_id} "{goal.title}" successfully deleted', 200)
+    create_message(f'Goal {goal_id} "{goal.title}" successfully deleted')
