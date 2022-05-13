e99c9b5b321437fd0a1ae70a57bad5a3ab2866ff
olive <olivealavine@gmail.com>
2022-05-12 22:57:48 -0500

    deleted commented out code

--- tests/test_wave_06.py
+++ tests/test_wave_06.py
@@ -50,11 +50,7 @@ def test_get_tasks_for_specific_goal_no_goal(client):
 
     # Assert
     assert response.status_code == 404
-
     #raise Exception("Complete test with assertion about response body")
-    # *****************************************************************
-    # **Complete test with assertion about response body***************
-    # *****************************************************************
     assert response_body == {'details': "No model of type <class 'app.models.goal.Goal'> with id 1 found"}
 
 
