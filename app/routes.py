25a04d5903c02005cccde65624df36292475f7d1
Marlyn <marlynlopezz@gmail.com>
2022-05-12 17:59:04 -0700

    refactored my goals response body

--- app/routes.py
+++ app/routes.py
@@ -66,12 +66,7 @@ def create_one_goal():
     db.session.add(new_goal)
     db.session.commit()
 
-    return {
-        "goal": {
-            "id": new_goal.goal_id,
-            "title": new_goal.title
-        }
-    }, 201 
+    return new_goal.to_dict(), 201 
 
 #READ
 #task get routes
@@ -97,7 +92,7 @@ def read_all_tasks():
 def read_one_task(task_id):
     chosen_task = get_task_or_abort(task_id)
     
-    response = { "task": chosen_task.to_dict()}
+    response = {"task": chosen_task.to_dict()}
 
     if chosen_task.goal_id:
         response ={ "task": {
@@ -133,12 +128,7 @@ def read_all_goals():
 def read_one_goal(goal_id):
     chosen_goal = get_goal_or_abort(goal_id)
     
-    response = { 
-        "goal": {
-                "id" : chosen_goal.goal_id,
-                "title": chosen_goal.title
-                }
-            }
+    response = chosen_goal.to_dict()
     return jsonify(response), 200
 
 
@@ -181,11 +171,7 @@ def replace_one_goal(goal_id):
 
     db.session.commit()
 
-    return { "goal": {
-                "id" : chosen_goal.goal_id,
-                "title": chosen_goal.title
-                }
-            }
+    return chosen_goal.to_dict()
     return jsonify(response), 200
 
 
@@ -213,7 +199,6 @@ def completed_task(task_id):
 
 
     return { "task": chosen_task.to_dict()}, 200
-    # return jsonify(response), 200
 
 
 @tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
