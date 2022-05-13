b12f7aeb0210e929c00334e61ab8a6b9dab1cd42
San <san.robinxin@gmail.com>
2022-05-14 04:27:56 +0800

    update deploy fail ...lol

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,25 @@
 from app import db
 
-
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    goal_key = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
+    
+    def dict(self):
+        return (dict(task={
+            "id" : self.task_id,
+            "title": self.title,
+            "description" : self.description,
+            "is_complete" : bool(self.completed_at),
+            "goal_id" : self.goal_key
+        }))
+
+    def to_dict(self):
+        return (dict(task={
+            "id" : self.task_id,
+            "title": self.title,
+            "description" : self.description,
+            "is_complete" : bool(self.completed_at)
+        }))
