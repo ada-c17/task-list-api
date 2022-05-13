0309358529df5e3ab38b8a560eb3e6e3d82b75d7
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-12 19:52:43 -0500

    Add get_all_goals() endpoint

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -4,6 +4,7 @@ from app.models.goal import Goal
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
+# create a new goal
 @goals_bp.route("", methods=["POST"])
 def create_goal():
     request_body = request.get_json()
@@ -15,3 +16,11 @@ def create_goal():
     response_body = {"goal": new_goal.to_dict()}
 
     return make_response(jsonify(response_body), 201)
+
+# get all goals
+@goals_bp.route("", methods=["GET"])
+def get_all_goals():
+    goals = Goal.query.all()
+    goals_response = [goal.to_dict() for goal in goals]
+
+    return jsonify(goals_response)
