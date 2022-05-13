dc877759f29fbe0178c58cbf8654b57ff6899992
Sujin Shin <susan.shin8@gmail.com>
2022-05-13 09:32:48 -0400

    waves 1-3

--- app/models/task.py
+++ app/models/task.py
@@ -2,4 +2,40 @@ from app import db
 
 
 class Task(db.Model):
-    task_id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime)
+    is_complete = db.Column(db.Boolean, default = False)
+
+    #i'm confused by what this does, what does the output look like
+    @classmethod
+    def create_complete(cls, request_body):
+        new_task = cls(
+            title=request_body["title"],
+            description=request_body["description"],
+            completed_at=request_body["completed_at"]
+        )
+
+        return new_task
+    
+    @classmethod
+    def create_not_complete(cls, request_body):
+        new_task = cls(
+            title=request_body["title"],
+            description=request_body["description"],
+            is_complete=request_body["is_complete"]
+        )
+
+        return new_task
+    
+    def to_json(self):
+        return {
+            "task": 
+            {
+            "id": self.task_id,
+            "title": self.title,
+            "description": self.description,
+            "is_complete": self.is_complete
+            }
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,199 @@
-from flask import Blueprint
+from os import abort
+from app import db
+from app.models.task import Task
+from flask import Blueprint, jsonify, abort, make_response, request
+from sqlalchemy import desc
+
+import time
+from datetime import date, datetime
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message":f"task {task_id} invalid"}, 400))
+
+    task = Task.query.get(task_id)
+
+    if not task:
+        abort(make_response({"message":f"task {task_id} not found"}, 404))
+
+    return task
+
+
+
+@tasks_bp.route("", methods=["POST"])
+def create_task():
+    request_body = request.get_json()
+    
+    #1st try
+    try:
+        request_body["completed_at"]
+    except:
+        request_body["completed_at"] = 0
+
+    #2nd try
+    try:
+        request_body["description"]
+        request_body["title"]
+    except:
+        return {"details": "Invalid data"}, 400
+
+    #3rd try
+    try:
+        request_body["is_complete"]
+    except:
+        request_body["is_complete"] = False
+
+    if request_body["completed_at"] != 0:
+        new_task = Task(title=request_body["title"],
+                        description=request_body["description"],
+                        completed_at= request_body["completed_at"],
+                        is_complete=True)
+
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return {
+            "task": {
+                "id": new_task.task_id,
+                "title": new_task.title,
+                "description": new_task.description,
+                "is_complete": new_task.is_complete
+            }
+        }, 201
+    
+    new_task = Task.create_not_complete(request_body)
+    
+    db.session.add(new_task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": new_task.task_id,
+            "title": new_task.title,
+            "description": new_task.description,
+            "is_complete": new_task.is_complete
+        }
+    }, 201
+    
+
+
+@tasks_bp.route("", methods=["GET"])
+def read_all_tasks():
+    sort_request = request.args.get("sort")
+    title_query = request.args.get("title")
+
+    tasks = Task.query.order_by(Task.title.asc()).all()
+
+    if sort_request == "asc":
+        tasks = Task.query.order_by(Task.title.asc()).all()
+    if sort_request == "desc":
+        tasks = Task.query.order_by(Task.title.desc()).all()
+    if title_query:
+        tasks = Task.query.filter_by(title=title_query)
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(
+            {
+                "id": task.task_id,
+                "title": task.title,
+                "description": task.description,
+                "is_complete": task.is_complete
+            }
+        )
+    return jsonify(tasks_response)
+
+
+
+@tasks_bp.route("/<task_id>", methods=["GET"])
+def read_one_task(task_id):
+    task = validate_task(task_id)
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete
+        }
+    }
+
+
+
+@tasks_bp.route("/<task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = validate_task(task_id)
+
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    if task.completed_at:
+        task.is_complete = True
+
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete
+        }
+    }
+
+
+@tasks_bp.route("/<task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return {"details": f"Task {task.task_id} \"{task.title}\" successfully deleted"}
+
+
+
+@tasks_bp.route("/<task_id>/mark_complete", methods=["PATCH"])
+def update_is_complete(task_id):
+    task = validate_task(task_id)
+
+    task.is_complete = True
+    task.completed_at = date.today()
+    
+    db.session.add(task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete
+        }
+    }, 200
+
+
+
+@tasks_bp.route("/<task_id>/mark_incomplete", methods=["PATCH"])
+def update_is_incomplete(task_id):
+    task = validate_task(task_id)
+
+    task.completed_at = None 
+
+    db.session.add(task)
+    db.session.commit()
+
+    return {
+        "task": {
+            "id": task.task_id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.is_complete
+        }
+    }, 200
