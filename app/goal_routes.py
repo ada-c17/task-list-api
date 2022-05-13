98512a163543b19ad2b44bd80e1d78aff8f296b0
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-12 20:15:49 -0500

    Create get_goal_by_id endpoint

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -28,17 +28,20 @@ def get_all_goals():
 # get goal by id
 @goals_bp.route("/<goal_id>", methods=["GET"])
 def get_goal_by_id(goal_id):
-    pass
+    goal = validate_goal(goal_id)
+    return {"goal": goal.to_dict()}
+
+
 
 def validate_goal(goal_id):
     try:
         goal_id = int(goal_id)
     except:
-        abort(make_response({"details": f"Goal '{goal_id}' invalid"}))
+        abort(make_response({"details": f"Goal #{goal_id} invalid"}, 400))
     
     goal = Goal.query.get(goal_id)
     
     if not goal:
-        abort(make_response({"details": f"Goal '{goal_id}' not found"}))
+        abort(make_response({"details": f"Goal #{goal_id} not found"}, 404))
     
     return goal
