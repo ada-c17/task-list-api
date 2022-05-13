1ac4162a3a7056549ae9e17bb6b5fd55094d2a4d
Hope Olaide <hopeolaide@gmail.com>
2022-05-13 03:20:33 -0400

    3 tests passed in Wave 6. Procfile and requirements updated.

--- app/backup_code.py
+++ app/backup_code.py
@@ -5,7 +5,10 @@ from .routes import validate_task
 from flask import Blueprint, request, jsonify, make_response, abort
 from datetime import datetime
 
+from .helper_routes import get_record_by_id
+
 goals_bp = Blueprint("goals", __name__, url_prefix="/goals")
+# ######
 
 # CREATE aka POST new goal at endpoint: /goals
 @goals_bp.route("", methods=["POST"])
@@ -14,55 +17,15 @@ def create_goal():
     if "title" not in request_body:
         return make_response(jsonify(dict(details="Invalid data")), 400)
     
-    new_goal = Goal.create(request_body)
+    goal = Goal.create(request_body)
+    # new_goal = Goal(title=request_body["title"],)
     
-    db.session.add(new_goal)
+    db.session.add(goal)
     db.session.commit()
 
-    return make_response(jsonify({"goal": new_goal.to_dict()}), 201)   
+    return make_response(jsonify({"goal": goal.to_dict()}), 201)   
 
-#######
-
-#app/goal_routes.py
-@goals_bp.route("/<goal_id>/tasks", methods=["POST"])
-def create_task_for_goal(goal_id):
-    goal = validate_goal(goal_id)
-    request_body = request.get_json()
-    
-    tasks_list = []
-
-    for task_id in request_body["task_ids"]:
-        task = validate_task(task_id)
-        task.goal = goal 
-        tasks_list.append(task.task_id)
-
-    db.session.commit()
-
-    return make_response(jsonify(dict(id=goal.goal_id, task_ids=tasks_list))), 200
-
-    # return make_response(jsonify(f"id: {task.title} for Goal: {task.goal.title} successfully created"), 200)
-### Confused about this??? ABOVE
-@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
-def get_tasks_per_goal(goal_id):
-    request_body = request.get_json()
-
-    goal = validate_goal(goal_id)
-    tasks_info = [task.todict(task) for task in goal.tasks]
-
-    # for task_id in request_body["task_ids"]:
-    #     tasks_list = []
-    #     task = validate_task(task_id)
-    #     # task.goal = goal 
-    #     tasks_list.append(task)
-
-    # goal = get_goal_by_id(Goal, goal_id)
-    # tasks_info = [Task.to_dict(task) for task in goal.tasks]
-    db.session.commit()
-
-    return jsonify(dict(id=goal.goal_id, title=goal.title, tasks=tasks_info)), 200
-
-
-    # GET ALL goalS aka READ at endpoint /goals
+# GET ALL goalS aka READ at endpoint /goals
 @goals_bp.route("", methods=["GET"])
 def read_all_goals():
     goals_response = []
@@ -81,6 +44,43 @@ def read_all_goals():
     goals_response = [goal.to_dict() for goal in goals]
 
     return make_response(jsonify(goals_response), 200) 
+
+###########
+
+#app/goal_routes.py
+@goals_bp.route("/<goal_id>/tasks", methods=["POST"])
+def find_task_for_goal(goal_id):
+    goal = validate_goal(goal_id)
+    # goal = get_record_by_id(Goal, goal_id)
+    request_body = request.get_json()
+    
+    tasks_list = []
+
+    for task_id in request_body["task_ids"]:
+        task = validate_task(task_id)
+        tasks_list.append(task)
+
+    [goal.tasks.append(task) for task in tasks_list if task not in goal.tasks]
+
+    db.session.commit()
+
+    return make_response(jsonify(dict(id=goal_id, task_ids=tasks_list))), 200
+    # return make_response(jsonify(f"id: {task.title} for Goal: {task.goal.title} successfully created"), 200)
+### Confused about this??? ABOVE
+
+
+# NOT SURE ABOUT BELOW
+@goals_bp.route("/<goal_id>/tasks", methods=["GET"])
+def get_tasks_per_goal(goal_id):
+
+    tasks_info = []
+    goal = validate_goal(goal_id)
+    goal = get_goal_by_id(Goal, goal_id)
+
+    tasks_info = [task.to_dict() for task in goal.tasks]
+
+
+    return jsonify(tasks_info)
 
 #####
 # GET aka READ goal at endpoint: /goals/id 
@@ -116,21 +116,24 @@ def delete_one_goal(id):
     return jsonify({'details': f'Goal {id} "{goal.title}" successfully deleted'}), 200
 
 #QUALITY CONTROL HELPER FUNCTION
-def validate_goal(id):
+def validate_goal(goal_id):
     try:
-        id = int(id)
+        goal_id = int(goal_id)
     except ValueError: 
         # return jsonify({}), 400     .....OR
         abort(make_response(jsonify(dict(details=f"invalid id: {id}")), 400))
+        # abort(make_response({"message":f"goal {goal_id} invalid"}, 400))
 
-    goal = Goal.query.get(id)
+    goal = Goal.query.get(goal_id)
     if goal:
         return goal
 
     elif not goal:
         abort(make_response(jsonify(dict(message= f"goal {id} not found")), 404))
-
+        # abort(make_response({"message":f"goal {goal_id} not found"}, 404))
     
+    return goal
+
 #########   
 # PATCH a goal at endpoint: goals/id  #Remember PATCH is just altering one or some attributes whereas PUT replaces a record. 
 @goals_bp.route("/<id>", methods=["PATCH"])
