104e0caf7768dbae7604b0e0c0c38ce07aacff7d
Raha <raha.mora1@gatech.edu>
2022-05-13 02:29:35 -0400

    remove flask_json

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
-from flask_json import FlaskJSON, JsonError, json_response, as_json
+
 
 db = SQLAlchemy()
 migrate = Migrate()
