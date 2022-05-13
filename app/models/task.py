d4bac3f80eae464ff9bd167d8896881ad0a1cb77
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-12 22:04:00 -0500

    Create one-to-many relationship between goals and tasks

--- app/models/task.py
+++ app/models/task.py
@@ -6,6 +6,8 @@ class Task(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable=True, default=None)
+    goal_id = db.Column(db.Integer, db.ForeignKey("goal.id"))
+    goal = db.relationship("Goal", back_populates="tasks")
 
     def to_dict(self):
         task_dict = {
