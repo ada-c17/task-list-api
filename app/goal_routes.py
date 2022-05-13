3ea0767bcf6a3f7880c67ad9fc6051310baf7531
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-12 19:30:19 -0500

    Add create_goal endpoint

--- app/goal_routes.py
+++ app/goal_routes.py
@@ -4,3 +4,14 @@ from app.models.goal import Goal
 
 goals_bp = Blueprint("goals_bp", __name__, url_prefix="/goals")
 
+@goals_bp.route("", methods=["POST"])
+def create_goal():
+    request_body = request.get_json()
+    new_goal = Goal(title=request_body["title"])
+
+    db.session.add(new_goal)
+    db.session.commit()
+
+    response_body = {"goal": new_goal.to_dict()}
+
+    return make_response(jsonify(response_body), 201)
