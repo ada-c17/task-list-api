efa726ad1f7880e4fde4627b19547e95240c6953
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-12 22:16:44 -0400

    Adds create class method to Goal class

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,9 +1,16 @@
 from app import db
+from app.commons import MissingValueError
 
 class Goal(db.Model):
     goal_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     tasks = db.relationship('Task', back_populates='goal')
+
+    @classmethod
+    def create(cls, goal_details):
+        if 'title' not in goal_details:
+            raise MissingValueError
+        return cls(title = goal_details['title'])
 
 
 class TasksGoal():
