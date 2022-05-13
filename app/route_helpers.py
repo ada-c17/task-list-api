147170aeb617a0de43e53fe3828f2b607173f2b8
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-13 18:22:05 -0500

    Create validate_model_instance() helper function

--- app/route_helpers.py
+++ app/route_helpers.py
@@ -1,5 +1,26 @@
 from flask import jsonify, make_response, abort
+from app.models.goal import Goal
+from app.models.task import Task
 
 # helper function to generate error message
 def error_message(message, status_code):
     abort(make_response(jsonify(dict(details=message)), status_code))
+
+# class/model-agnostic helper function to validate model instances
+def validate_model_instance(model, id):
+    if model == Task:
+        model_name = "Task"
+    elif model == Goal:
+        model_name = "Goal"
+    
+    try:
+        id = int(id)
+    except:
+        error_message(f"{model_name} #{id} invalid", 400)
+    
+    model_instance = model.query.get(id)
+
+    if not model_instance:
+        error_message(f"{model_name} #{id} not found", 404)
+    
+    return model_instance
