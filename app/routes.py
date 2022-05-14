e4148b2a491bd1330780d62fe6bce9fcced53d51
jae <j.kerr.clayton@gmail.com>
2022-05-13 19:34:29 -0500

    tried to make route for main page

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,9 @@ task_bp = Blueprint("tasks", __name__, url_prefix="/tasks")
 goal_bp = Blueprint("goals", __name__, url_prefix="/goals")
 main_bp = Blueprint("/", __name__)
 
-@main_bp.route("", method=["GET"])
-def main_page(): 
-    return ("Welcome to task-list -- A space to track tasks and goals")
+# @main_bp.route("", method=["GET"])
+# def main_page(): 
+#     return ("Welcome to task-list -- A space to track tasks and goals")
 
 
 
