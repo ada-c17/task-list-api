bac6ee6e193a83cc6616b14f3cd537cc2c04b5f4
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-05-13 19:36:35 -0500

    Refactor post_slack_completion_message helper function

--- app/route_helpers.py
+++ app/route_helpers.py
@@ -1,6 +1,7 @@
 from flask import jsonify, make_response, abort
 from app.models.goal import Goal
 from app.models.task import Task
+import requests, os
 
 # helper function to generate error message
 def error_message(message, status_code):
@@ -24,3 +25,17 @@ def validate_model_instance(model, id):
         error_message(f"{model_name} #{id} not found", 404)
     
     return model_instance
+
+# helper function to post completion message to slack
+def post_slack_completion_message(task_id):
+    task = validate_model_instance(Task, task_id)
+    path = "https://slack.com/api/chat.postMessage"
+    SLACK_API_KEY = os.environ.get("SLACK_BOT_TOKEN")
+
+    request_headers = {"Authorization": f"Bearer {SLACK_API_KEY}"}
+    request_body = {
+        "channel": "C03EP2Q0WK1",
+        "text": f"Someone just completed the task {task.title}"
+    }
+
+    requests.post(path, headers=request_headers, json=request_body)
