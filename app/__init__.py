6220305a59fa1d8b2c0d4a28e9e3fe3a4bc20bd9
Shayla Logan <shayla.logan709@gmail.com>
2022-05-17 11:40:13 -0700

    removed a duplicate import

--- app/__init__.py
+++ app/__init__.py
@@ -30,7 +30,6 @@ def create_app(test_config=None):
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    from app.models.task import Task
     from .routes.task_routes import task_bp
     from .routes.goal_routes import goal_bp
     app.register_blueprint(task_bp)
