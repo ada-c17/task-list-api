ef7c1021bd8bb065a32b190ac2b9b4272ddfdace
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-21 11:14:37 -0400

    Adds goal maniupulation to CLI and updates url to work with deployment.

--- cli/main.py
+++ cli/main.py
@@ -1,16 +1,17 @@
 import task_list
 
 OPTIONS = {
-        "1": "List all tasks", 
-        "2": "Create a task",
-        "3": "View one task", 
-        "4": "Update task", 
-        "5": "Delete task", 
+        "1": "View all", 
+        "2": "Create",
+        "3": "View one", 
+        "4": "Update", 
+        "5": "Delete", 
         "6": "Mark complete",
         "7": "Mark incomplete",
-        "8": "Delete all tasks",
+        "8": "Delete all",
         "9": "List all options",
-        "10": "Quit"
+        "10": "Quit",
+        "A": "Assign task(s) to goal" 
         }
 
 def list_options():
@@ -21,50 +22,63 @@ def list_options():
 
 def make_choice():
     valid_choices = OPTIONS.keys()
+    valid_resources = {'t', 'g'}
     choice = None
+    resource = None
 
     while choice not in valid_choices:
         print("\n What would you like to do? ")
-        choice = input("Make your selection using the option number. Select 9 to list all options: ")
+        choice = input("Make your selection using the option number. "
+                        "Select 9 to list all options: ").upper()
+    
+    if choice == 'A':
+        resource = 'g'
+    elif choice in {'6', '7', '9', '10'}:
+        resource = 't'
+    while resource not in valid_resources:
+        resource = input('Ok, are we working with tasks or goals? '
+                        '(Enter "T" or "G"): ').lower()
+            
+    return choice, resource
 
-    return choice
-
-def get_task_from_user(msg = "Input the id of the task you would like to work with: "):
-    task = None
-    tasks = task_list.list_tasks()
-    if not tasks:
-        task_list.print_stars("This option is not possible because there are no tasks.")
-        return task
+def get_id_from_user(resource, msg = "Input the id of the item you would like to work with: "):
+    item = None
+    items = task_list.list_tasks() if resource == 't' else task_list.list_goals()
+    if not items:
+        task_list.print_stars("This option is not possible because there are none found.")
+        return
     count = 0
     help_count = 3 #number of tries before offering assistance
-    while not task:
+    while not item:
         id = input(msg)
-        task = task_list.get_task(id)
-        if not task:
-            print_surround_stars("I cannot find that task.  Please try again.")
+        item = task_list.get_task(id) if resource == 't' else task_list.get_goal(id)
+        if not item:
+            print_surround_stars("I cannot find that item.  Please try again.")
         count += 1
         if count >= help_count:
-            print("You seem to be having trouble selecting a task.  Please choose from the following list of tasks.")
-            print_all_tasks()
+            print("You seem to be having trouble selecting an item.  Please choose from the following list.")
+            print_all(resource)
         
-    return task
+    return item
 
-def print_task(task):
+def print_item(item):
     print_single_row_of_stars()
-    print("title: ", task["title"])
-    print("description: ", task["description"])
-    print("is_complete: ", task["is_complete"])
-    print("id: ", task["id"])
+    for key in item:
+        print(f'{key}: {item[key]}')
     print_single_row_of_stars()
 
-def print_all_tasks():
-    tasks = task_list.list_tasks()
-    print("\nTasks:")
-    if not tasks:
-        print_surround_stars("No tasks")
+def print_all(resource):
+    if resource == 't': 
+        items = task_list.list_tasks()
+        print("\nTasks:")
     else:
-        for task in tasks:
-            print_task(task)
+        items = task_list.list_goals()
+        print("\nGoals:")
+    if not items:
+        print_surround_stars("None found")
+    else:
+        for item in items:
+            print_item(item)
     print_single_row_of_stars()
 
 def print_surround_stars(sentence):
@@ -75,52 +89,73 @@ def print_surround_stars(sentence):
 def print_single_row_of_stars():
     print("\n**************************\n")
 
-def create_task():
-    print("Great! Let's create a new task.")
-    title=input("What is the title of your task? ")
-    description=input("What is the description of your task? ")
-    response = task_list.create_task(title, description)
-    print_task(response)
+def create(resource):
+    print("Great! Let's create a new resource.")
+    title=input("What is its title? ")
+    if resource == 't':
+        description=input("What is the description of your task? ")
+        response = task_list.create_task(title, description)
+    else:
+        response = task_list.create_goal(title)
+    print_item(response)
 
-def view_task():
-    task = get_task_from_user("Input the id of the task you would like to select ")
-    if task: 
-        print("\nSelected Task:")
-        print_task(task)
+def view(resource):
+    item = get_id_from_user(resource, "Input the id of the task you would like to select ")
+    if item: 
+        print("\nSelected Item:")
+        print_item(item)
 
-def edit_task():
-    task = get_task_from_user()
-    if task:
-        title=input("What is the new title of your task? ")
-        description=input("What is the new description of your task? ")
-        response = task_list.update_task(task["id"], title, description)
-        print("\nUpdated Task:")
-        print_task(response)
+def edit(resource):
+    item = get_id_from_user(resource)
+    if item:
+        title=input("What is the new title? ")
+        if resource == 't':
+            description=input("What is the new description of your task? ")
+            response = task_list.update_task(item["id"], title, description)
+            print("\nUpdated Task:")
+        else:
+            response = task_list.update_goal(item["id"], title)
+            print("\nUpdated Goal:")
+        print_item(response)
 
-def delete_task_ui():
-    task = get_task_from_user("Input the id of the task you would like to delete: ")
-    if task:
-        task_list.delete_task(task["id"])
-        print("\nTask has been deleted.")
-        print_all_tasks()
+def delete(resource):
+    item = get_id_from_user(resource, "Input the id of the item you would like to delete: ")
+    if item:
+        if resource == 't':
+            task_list.delete_task(item["id"])
+        else:
+            task_list.delete_goal(item["id"])
+        print("\nItem has been deleted.")
+        print_all(resource)
 
 def change_task_complete_status(status):
     status_text = "complete"
     if not status:
         status_text = "incomplete"
-    task = get_task_from_user(f"Input the id of the task you would like to mark {status_text}: ")
+    task = get_id_from_user('t', f"Input the id of the task you would like to mark {status_text}: ")
     if task:
         if status:
             response = task_list.mark_complete(task["id"])
         else:
             response = task_list.mark_incomplete(task["id"])
         print(f"\nTask marked {status_text}:")
-        print_task(response)
+        print_item(response)
 
-def delete_all_tasks():
-    for task in task_list.list_tasks():
-        task_list.delete_task(task["id"])
-        print_surround_stars("Deleted all tasks.")
+def delete_all(resource):
+    if resource == 't':
+        methods = ['list_tasks', 'delete_task']
+    else:
+        methods = ['list_goals', 'delete_goal']
+    for item in getattr(task_list, methods[0])():
+        getattr(task_list, methods[1])(item["id"])
+        print_surround_stars("Deleted all items.")
+
+def assign_to_goal():
+    goal = get_id_from_user('g', f"Input the id of the goal you would like to assign tasks to: ")
+    task_ids = input('Input the ids of the tasks you would like to assign, separated by spaces (ex: "1 2 3"): ').split()
+    response = task_list.assign_tasks_to_goal(goal['id'], task_ids)
+
+    print_item(goal)
 
 def run_cli():
     
@@ -130,26 +165,28 @@ def run_cli():
         # get input and validate
         choice = make_choice()
 
-        if choice=='1':
-            print_all_tasks()
-        elif choice=='2':
-            create_task()
-        elif choice=='3':
-            view_task()
-        elif choice=='4':
-            edit_task()
-        elif choice=='5':
-            delete_task_ui()
-        elif choice=='6':
+        if choice[0]=='1':
+            print_all(choice[1])
+        elif choice[0]=='2':
+            create(choice[1])
+        elif choice[0]=='3':
+            view(choice[1])
+        elif choice[0]=='4':
+            edit(choice[1])
+        elif choice[0]=='5':
+            delete(choice[1])
+        elif choice[0]=='6':
             change_task_complete_status(True)
-        elif choice=='7':
+        elif choice[0]=='7':
             change_task_complete_status(False)
-        elif choice=='8':
-            delete_all_tasks()
-        elif choice=='9':
+        elif choice[0]=='8':
+            delete_all(choice[1])
+        elif choice[0]=='9':
             list_options()
-        elif choice=='10':
+        elif choice[0]=='10':
             play=False
+        elif choice[0]=='A':
+            assign_to_goal()
 
 
 print("Welcome to Task List CLI")
--- cli/task_list.py
+++ cli/task_list.py
@@ -1,12 +1,13 @@
 import requests
+import os
 
-url = "http://localhost:5000"
+url = f"https://{os.environ.get('api')}"
 
 def parse_response(response):
     if response.status_code >= 400:
         return None
     
-    return response.json()["task"]
+    return list(response.json().values())[0]
 
 def create_task(title, description, completed_at=None):
     query_params = {
@@ -54,6 +55,50 @@ def mark_incomplete(id):
     response = requests.patch(url+f"/tasks/{id}/mark_incomplete")
     return parse_response(response)
 
+def create_goal(title):
+    query_params = {
+        "title": title
+    }
+    response = requests.post(url+"/goals",json=query_params)
+    return parse_response(response)
 
+def list_goals():
+    response = requests.get(url+"/goals")
+    return response.json()
 
+def get_goal(id):
+    response = requests.get(url+f"/goals/{id}")
+    if response.status_code != 200:
+        return None
+        
+    return parse_response(response)
+
+def update_goal(id,title):
+
+    query_params = {
+        "title": title
+    }
+
+    response = requests.put(
+        url+f"/goals/{id}",
+        json=query_params
+        )
     
+    return parse_response(response)
+
+def delete_goal(id):
+    response = requests.delete(url+f"/goals/{id}")
+    return response.json()
+
+def assign_tasks_to_goal(id, task_ids):
+
+    query_params = {
+        "task_ids": task_ids
+    }
+
+    response = requests.post(
+        url+f"/goals/{id}/tasks",
+        json=query_params
+        )
+    
+    return parse_response(response)
