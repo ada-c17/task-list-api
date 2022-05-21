8794a2785f7ac70e495272fa47262e60baf3a0ab
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-05-21 17:42:58 -0400

    Adds error message to debug failing button interaction.

--- app/routes.py
+++ app/routes.py
@@ -262,7 +262,10 @@ def respond_to_bot() -> tuple[Response, Literal[200]]:
                     '/addtask', '/alltasks', '/addtogoal'}
     if data.get('payload', None):
         payload = json.loads(data['payload'])
-        task_id = payload['value']
+        try:
+            task_id = payload['value']
+        except:
+            abort(make_error_response(KeyError, None, detail=(f' payload = {payload}')))
         action = payload['action_id'].split()
         try:
             task = validate_and_get_by_id(Task, task_id)
