88f1d991b69e0fb21e9a81c243be9bfe35e6b3c0
Katina Carranza <katina.carranza@gmail.com>
2022-05-22 15:29:25 -0700

    Completion on Heroku Deployment

--- app/models/goal.py
+++ app/models/goal.py
@@ -6,17 +6,26 @@ class Goal(db.Model):
     title = db.Column(db.String)
     tasks = db.relationship('Task',backref="goal",lazy=True)
 
+    def __repr__(self) -> str:
+        return str(self)
+        
+    def __str__(self) -> str:
+        return f'Goal ID {self.goal_id}, Title {self.title}, Tasks{self.tasks}'
+
     def to_dict(self):
+        tasks_info = [task.to_dict() for task in self.tasks]
         return dict(
             id=self.goal_id,
             title=self.title,
+            # tasks=tasks_info
     )
 
     @classmethod
     def from_dict(cls, data_dict):
-        return cls(
-            title=data_dict['title'],
-        )
+        return Goal(title=data_dict["title"]) 
+        # cls(
+            # title=data_dict['title'],
+        # )
 
     def replace_details(self, data_dict):
         self.title=data_dict['title']
--- app/models/task.py
+++ app/models/task.py
@@ -7,13 +7,16 @@ class Task(db.Model):
     description = db.Column(db.String)
     completed_at = db.Column(db.DateTime, nullable = True)
     goal_id = db.Column(db.Integer, db.ForeignKey('goal.goal_id'), nullable=True)
-
+    def __repr__(self) -> str:
+        return str(self)
+    def __str__(self) -> str:
+        return f'Task ID {self.task_id}, Title {self.title}, Completed At{self.completed_at}'
     def to_dict(self):
         return dict(
             id=self.task_id,
             title=self.title,
             description=self.description,
-            is_complete=False
+            is_complete=self.completed_at is not None
         
     )
 
@@ -21,9 +24,10 @@ class Task(db.Model):
     def from_dict(cls, data_dict):
         return cls(
             title=data_dict['title'],
-            description=data_dict['description']
-            # completed_at=data_dict['completed_at']
+            description=data_dict['description'],
+            completed_at=data_dict.get("completed_at", None)
         )
+        
 
     def replace_details(self, data_dict):
         self.title=data_dict['title']
