4ec20600c50ecba4383c3dee0ae03a13c816c560
Celina <armchairgraduate@gmail.com>
2022-06-09 03:43:16 -0700

    wave 1 complete, all tests pass

--- app/__init__.py
+++ app/__init__.py
@@ -31,4 +31,6 @@ def create_app(test_config=None):
 
     # Register Blueprints here
 
+    from .routes import tasks_bp
+    app.register_blueprint(tasks_bp)
     return app
--- app/models/task.py
+++ app/models/task.py
@@ -3,3 +3,20 @@ from app import db
 
 class Task(db.Model):
     task_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def to_dictionary(self):
+        return dict(
+            id=self.task_id,
+            title=self.title,
+            description=self.description,
+            is_complete=self.is_complete()
+        )
+    
+    def is_complete(self):
+        if not self.completed_at:
+            return False
+        else: 
+            return True
